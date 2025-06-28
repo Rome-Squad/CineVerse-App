a416fdefc6f8342ee528af5fa26f2a86f194deb4
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-06-28 18:54:41 +0300

    add text style to singelton to access it direct

--- common/presentation/src/main/java/com/giraffe/presentation/designsystem/theme/Theme.kt
+++ common/presentation/src/main/java/com/giraffe/presentation/designsystem/theme/Theme.kt
@@ -4,9 +4,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import com.giraffe.presentation.designsystem.color.CineVerseColors
 import com.giraffe.presentation.designsystem.color.LocalCineVerseColors
+import com.giraffe.presentation.designsystem.text_style.CineVerseTextStyle
+import com.giraffe.presentation.designsystem.text_style.LocalCineVerseTextStyle
 
 object Theme {
     val color: CineVerseColors
         @Composable @ReadOnlyComposable get() = LocalCineVerseColors.current
-    
+
+    val textStyle: CineVerseTextStyle
+        @Composable @ReadOnlyComposable get() = LocalCineVerseTextStyle.current
 }
