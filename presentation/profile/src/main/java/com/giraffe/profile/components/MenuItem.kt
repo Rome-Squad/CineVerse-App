c927b16b3f4776c80d6f9770fcdefc3febafc1dc
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-06-29 02:57:53 +0300

    feat: add bottom divider

--- presentation/profile/src/main/java/com/giraffe/profile/components/MenuItem.kt
+++ presentation/profile/src/main/java/com/giraffe/profile/components/MenuItem.kt
@@ -1,13 +1,16 @@
 package com.giraffe.profile.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Icon
+import androidx.compose.material.TabRowDefaults.Divider
 import androidx.compose.material.Text
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -25,47 +28,57 @@ fun MenuItem(
     hasButton: Boolean,
     onButtonClick: () -> Unit,
     isDanger: Boolean = false,
+    hasBottomDivider: Boolean = false
 ) {
-    Row(
+    Column(
         modifier = modifier
-            .size(48.dp)
-            .background(Theme.color.shade.quaternary!!),
-        horizontalArrangement = Arrangement.SpaceBetween,
-        verticalAlignment = androidx.compose.ui.Alignment.CenterVertically
+            .background(Theme.color.shade.quaternary!!)
     ) {
         Row(
-            horizontalArrangement = Arrangement.Start,
+            horizontalArrangement = Arrangement.SpaceBetween,
+            verticalAlignment = androidx.compose.ui.Alignment.CenterVertically,
         ) {
-            Icon(
-                painter = painterResource(id = icon),
-                contentDescription = title,
-                tint = if (isDanger) Theme.color.additional.primary.red else Theme.color.shade.primary,
-                modifier = Modifier
-                    .size(24.dp)
-            )
-            Text(
-                text = title,
-                modifier = Modifier
-                    .weight(1f),
-                color = if (isDanger) Theme.color.shade.quaternary!! else Theme.color.shade.primary,
-                fontStyle = Theme.textStyle.body.medium.medium.fontStyle,
-            )
-        }
-        Row(
-            horizontalArrangement = Arrangement.End,
-        ) {
-            if (hasSwitch) {
-                // switch from design system
-            }
-            if (hasButton) {
+            Row(
+                horizontalArrangement = Arrangement.Start,
+            ) {
                 Icon(
-                    painter = painterResource(id = CineVerseIcons().outline.altArrowRight),
-                    contentDescription = "Navigate to $title",
+                    painter = painterResource(id = icon),
+                    contentDescription = title,
+                    tint = if (isDanger) Theme.color.additional.primary.red else Theme.color.shade.primary,
                     modifier = Modifier
                         .size(24.dp)
-                        .clickable { onButtonClick() }
+                )
+                Text(
+                    text = title,
+                    modifier = Modifier
+                        .weight(1f),
+                    color = if (isDanger) Theme.color.additional.primary.red else Theme.color.shade.primary,
+                    style = Theme.textStyle.body.medium.medium,
                 )
             }
+            Row(
+                horizontalArrangement = Arrangement.End,
+            ) {
+                if (hasSwitch) {
+                    // switch from design system
+                }
+                if (hasButton) {
+                    Icon(
+                        painter = painterResource(id = CineVerseIcons().outline.altArrowRight),
+                        contentDescription = "Navigate to $title",
+                        tint = if (isDanger) Theme.color.additional.primary.red else Theme.color.shade.primary,
+                        modifier = Modifier
+                            .size(24.dp)
+                            .clickable { onButtonClick() }
+                    )
+                }
+            }
+        }
+        if (hasBottomDivider) {
+            Divider(
+                color = Theme.color.shade.tertiary!!,
+                thickness = 1.dp
+            )
         }
     }
 }
