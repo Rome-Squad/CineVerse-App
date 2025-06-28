e857035c1df54123bbfc7eee7d909d2f111cf1f2
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-06-28 19:10:36 +0300

    add radius local compostion to singelton to access it

--- common/presentation/src/main/java/com/giraffe/presentation/designsystem/theme/Theme.kt
+++ common/presentation/src/main/java/com/giraffe/presentation/designsystem/theme/Theme.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
 import com.giraffe.presentation.designsystem.color.CineVerseColors
 import com.giraffe.presentation.designsystem.color.LocalCineVerseColors
+import com.giraffe.presentation.designsystem.radius.CineVerseRadius
+import com.giraffe.presentation.designsystem.radius.LocalCineVerseRadius
 import com.giraffe.presentation.designsystem.text_style.CineVerseTextStyle
 import com.giraffe.presentation.designsystem.text_style.LocalCineVerseTextStyle
 
@@ -13,4 +15,7 @@ object Theme {
 
     val textStyle: CineVerseTextStyle
         @Composable @ReadOnlyComposable get() = LocalCineVerseTextStyle.current
+
+    val radius: CineVerseRadius
+        @Composable @ReadOnlyComposable get() = LocalCineVerseRadius.current
 }
