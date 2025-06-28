deda6a4bb3debe30cef215e8a23dc4e88886c355
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-06-28 18:26:23 +0300

    add text style data class

---
+++ common/presentation/src/main/java/com/giraffe/presentation/designsystem/text_style/CineVerseTextStyle.kt
@@ -0,0 +1,23 @@
+package com.giraffe.presentation.designsystem.text_style
+
+import androidx.compose.ui.text.TextStyle
+
+data class CineVerseTextStyle(
+    val displayXl: TextStyle,
+    val titleXl: TextStyle,
+    val titleLg: TextStyle,
+    val titleMd: TextStyle,
+    val titleSm: TextStyle,
+    val bodyLgRegular: TextStyle,
+    val bodyLgMedium: TextStyle,
+    val bodyLgSemiBold: TextStyle,
+    val bodyMdRegular: TextStyle,
+    val bodyMdMedium: TextStyle,
+    val bodyMdSemiBold: TextStyle,
+    val bodySmRegular: TextStyle,
+    val bodySmMedium: TextStyle,
+    val bodySmSemiBold: TextStyle,
+    val labelMdRegular: TextStyle,
+    val labelMdMedium: TextStyle,
+    val labelMdSemiBold: TextStyle
+)
