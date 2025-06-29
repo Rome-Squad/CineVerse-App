a92a03e1182c57be5251fbd68ab040220869fd79
bilalazzam <blalazam065@gmail.com>
2025-06-29 05:45:52 +0300

    fix and preview RatingStars

--- app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
+++ app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
@@ -4,13 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.giraffe.cineverseapp.components.RatingStatrs
 import com.giraffe.cineverseapp.ui.theme.CineVerseAppTheme
 import com.giraffe.presentation.designsystem.theme.CinVerseTheme
 
@@ -19,13 +27,39 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var rate by remember {
+                mutableIntStateOf(
+                    0
+                )
+            }
             CinVerseTheme {
+
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column {
+
+                        Greeting(
+                            name = "Android",
+                            modifier = Modifier.padding(
+                                innerPadding
+                            )
+
+                        )
+
+                        Row {  }
+
+                        RatingStatrs(
+                            modifier = Modifier,
+                            rate = rate,
+                            onRateClickEnabled = true,
+                            onRateClick = {
+                                rate = it
+                            }
+                        )
+
+                    }
+
                 }
+
             }
         }
     }
--- presentation/details/src/main/java/com/giraffe/cineverseapp/components/RatingStatrsItem.kt
+++ presentation/details/src/main/java/com/giraffe/cineverseapp/components/RatingStatrsItem.kt
@@ -1,21 +1,28 @@
 package com.giraffe.cineverseapp.components
 
 
+import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.Row
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giraffe.presentation.R
+import com.giraffe.presentation.designsystem.theme.CinVerseTheme
+import com.giraffe.presentation.designsystem.theme.Theme
 
 
 @Composable
-fun RatingStatrsItem(
+fun RatingStatrs(
     modifier: Modifier = Modifier,
     rate: Int = 0,
     onRateClickEnabled: Boolean = true,
@@ -27,23 +34,27 @@ fun RatingStatrsItem(
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         verticalAlignment = Alignment.CenterVertically
     ) {
-
         for (i in 0 until 5) {
 
             Icon(
                 painter = painterResource(
+
                     id = if (i < rate)
                         R.drawable.bold_star
                     else
                         R.drawable.outline_star
+
                 ),
+                tint = Theme.color.additional.primary.yellow,
                 contentDescription = "rate $rate",
                 modifier = Modifier
                     .then(
+
                         if (onRateClickEnabled)
                             Modifier.clickable { onRateClick(i + 1) }
                         else
                             Modifier
+
                     )
 
             )
@@ -52,4 +63,53 @@ fun RatingStatrsItem(
 
     }
 
+}
+
+
+@Composable
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
+fun PreviewRatingStars() {
+
+    var rate by remember {
+        mutableIntStateOf(
+            0
+        )
+    }
+
+    CinVerseTheme(
+        isDarkTheme = true
+    ) {
+        RatingStatrs(
+            rate = rate
+        ) {
+            rate = it
+        }
+    }
+}
+
+@Composable
+@Preview(
+    showBackground = true,
+    showSystemUi = false
+)
+fun PreviewRatingStarsNight() {
+
+    var rate by remember {
+        mutableIntStateOf(
+            0
+        )
+    }
+
+    CinVerseTheme(
+        isDarkTheme = false
+    ) {
+        RatingStatrs(
+            rate = rate
+        ) {
+            rate = it
+        }
+    }
 }
