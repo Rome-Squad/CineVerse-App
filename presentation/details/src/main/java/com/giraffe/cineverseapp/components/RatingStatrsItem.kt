550562792f7dd9d83afe1ff63121e3096d2975f3
bilalazzam <blalazam065@gmail.com>
2025-06-29 05:46:21 +0300

    fix and preview RatingStars

--- presentation/details/src/main/java/com/giraffe/cineverseapp/components/RatingStatrsItem.kt
+++ presentation/details/src/main/java/com/giraffe/cineverseapp/components/RatingStatrsItem.kt
@@ -1,7 +1,6 @@
 package com.giraffe.cineverseapp.components
 
 
-import android.content.res.Configuration
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
