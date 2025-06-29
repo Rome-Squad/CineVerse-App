f77b9bc6e600488af7c9ce858372b2e56171704f
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-06-29 13:19:42 +0300

    create season card

---
+++ presentation/details/src/main/java/com/giraffe/cineverseapp/components/SeasonCard.kt
@@ -0,0 +1,187 @@
+package com.giraffe.cineverseapp.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import com.giraffe.presentation.designsystem.theme.CinVerseTheme
+import com.giraffe.presentation.designsystem.theme.Theme
+
+@Composable
+fun SeasonCard(
+    modifier: Modifier = Modifier,
+    poster: Painter,
+    title: String,
+    caption: String,
+    rating: Double,
+    episodes: Int,
+    year: Int,
+    posterWidth: Dp = 48.dp,
+    posterHeight: Dp = 64.dp
+) {
+    Card(
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(12.dp),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(Theme.color.background.card)
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(12.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = poster,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(width = posterWidth, height = posterHeight)
+                        .clip(
+                            RoundedCornerShape(
+                                topStart = 32.dp,
+                                topEnd = 32.dp,
+                                bottomEnd = 4.dp,
+                                bottomStart = 4.dp
+                            )
+                        ),
+                    contentScale = ContentScale.Crop
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Column(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Text(
+                        text = title,
+                        style = Theme.textStyle.body.medium.medium,
+                        color = Theme.color.shade.primary
+                    )
+                    Text(
+                        text = caption,
+                        style = Theme.textStyle.body.small.regular,
+                        color = Theme.color.shade.secondary
+                    )
+                }
+            }
+
+            Spacer(modifier = Modifier.height(12.dp))
+            HorizontalDivider(thickness = 0.5.dp, color = Theme.color.stroke.primary)
+            Spacer(modifier = Modifier.height(12.dp))
+
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.Center
+            ) {
+                Icon(
+                    painter = painterResource(id = Theme.icons.dueTone.star),
+                    tint = Theme.color.additional.primary.yellow,
+                    contentDescription = "Rating",
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = rating.toString(),
+                    color = Theme.color.shade.secondary,
+                    style = Theme.textStyle.label.regular,
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Icon(
+                    painter = painterResource(id = Theme.icons.dueTone.videoLibrary),
+                    contentDescription = "Episodes",
+                    tint = Theme.color.shade.secondary,
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "$episodes Episodes",
+                    color = Theme.color.shade.secondary,
+                    style = Theme.textStyle.label.regular,
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Icon(
+                    painter = painterResource(id = Theme.icons.dueTone.calendar),
+                    contentDescription = "Year",
+                    tint = Theme.color.shade.secondary,
+                    modifier = Modifier.size(16.dp)
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "$year",
+                    color = Theme.color.shade.secondary,
+                    style = Theme.textStyle.label.regular,
+                )
+            }
+        }
+    }
+}
+@Preview(
+    name = "SeasonCard Dark",
+    apiLevel = 34,
+    showBackground = true,
+    showSystemUi = false
+)
+@Composable
+fun PreviewSeasonCardDark() {
+    CinVerseTheme(isDarkTheme = true) {
+        SeasonCard(
+            poster = painterResource(id = com.giraffe.presentation.R.drawable.poster_example),
+            title = "Season",
+            caption = "Caption",
+            rating = 7.5,
+            episodes = 20,
+            year = 2019
+        )
+    }
+}
+
+@Preview(
+    name = "SeasonCard Light",
+    apiLevel = 34,
+    showBackground = true,
+    showSystemUi = false
+)
+@Composable
+fun PreviewSeasonCardLight() {
+    CinVerseTheme(isDarkTheme = false) {
+        SeasonCard(
+            poster = painterResource(id = com.giraffe.presentation.R.drawable.poster_example),
+            title = "Season",
+            caption = "Caption",
+            rating = 7.5,
+            episodes = 20,
+            year = 2019
+        )
+    }
+}
