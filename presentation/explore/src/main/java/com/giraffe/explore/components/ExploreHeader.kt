4ccf98908acb89bbcceafff034fde03a79ed0820
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-07-01 02:34:41 +0300

    feat: add dynamic end icon and back button

--- presentation/explore/src/main/java/com/giraffe/explore/components/ExploreHeader.kt
+++ presentation/explore/src/main/java/com/giraffe/explore/components/ExploreHeader.kt
@@ -2,6 +2,8 @@ package com.giraffe.explore.components
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsFocusedAsState
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -9,9 +11,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -22,30 +27,52 @@ import com.giraffe.designsystem.theme.Theme
 @Composable
 fun ExploreHeader(
     modifier: Modifier = Modifier,
-    title: String = "Explore",
     onBackClick: () -> Unit = {},
     onSearchClick: (String) -> Unit = {},
-    endIcon: @Composable (() -> Unit)? = null
 ) {
+    val interactionSource = remember { MutableInteractionSource() }
+    val isFocused by interactionSource.collectIsFocusedAsState()
+    val focusManager = LocalFocusManager.current
+
+
+    val endIcon: @Composable (() -> Unit) = {
+        if (isFocused) {
+            EndIcon(
+                icon = painterResource(Theme.icons.outline.close),
+                onClick = {
+                    focusManager.clearFocus()
+                },
+                contentDescription = "Cancel Search"
+            )
+        } else {
+            EndIcon(
+                icon = painterResource(Theme.icons.outline.microphone),
+                onClick = { },
+                contentDescription = "Microphone"
+            )
+        }
+    }
 
     Column(
         modifier = modifier
             .background(color = Theme.color.background.screen),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
-        Row (
+        Row(
             modifier = Modifier,
             verticalAlignment = Alignment.CenterVertically
         ) {
-            Icon(
-                modifier = Modifier
-                    .padding(8.dp)
-                    .size(20.dp)
-                    .clickable(onClick = onBackClick),
-                painter = painterResource(Theme.icons.outline.arrowLeft),
-                contentDescription = "Back",
-                tint = Theme.color.shade.primary
-            )
+            if (isFocused) {
+                Icon(
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .size(20.dp)
+                        .clickable(onClick = onBackClick),
+                    painter = painterResource(Theme.icons.outline.arrowLeft),
+                    contentDescription = "Back",
+                    tint = Theme.color.shade.primary
+                )
+            }
             AppTextField(
                 modifier = modifier,
                 startIcon = painterResource(Theme.icons.outline.search),
@@ -81,16 +108,9 @@ fun EndIcon(
 @Preview
 @Composable
 fun ExploreHeaderPreview() {
-    CineVerseTheme (isDarkTheme = true){
+    CineVerseTheme(isDarkTheme = true) {
         ExploreHeader(
-            title = "Explore",
             onBackClick = {},
-            endIcon = {
-                EndIcon(
-                    icon = painterResource(Theme.icons.outline.microphone),
-                    onClick = {}
-                )
-            }
         )
     }
 }
