a1f4c9a4860e947e809911ef3a6f41ef988cae2b
Mohannad Ahmed <mohannad199344@gmail.com>
2025-06-30 05:59:47 +0300

    feat: add presentation module dependency for home component

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -41,6 +41,7 @@ android {
 
 dependencies {
     implementation(project(":designsystem"))
+    implementation(project(":presentation:home"))
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
