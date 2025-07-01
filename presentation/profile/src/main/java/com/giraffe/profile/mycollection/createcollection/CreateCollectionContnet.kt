74856ee31c1cb52b59262d4635bb34ffe00ba653
HendSayed25 <hendsayed853@gmail.com>
2025-07-01 18:47:08 +0300

    add label

--- presentation/profile/src/main/java/com/giraffe/profile/mycollection/createcollection/CreateCollectionContnet.kt
+++ presentation/profile/src/main/java/com/giraffe/profile/mycollection/createcollection/CreateCollectionContnet.kt
@@ -2,11 +2,15 @@ package com.giraffe.profile.mycollection.createcollection
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.giraffe.designsystem.R
+import com.giraffe.designsystem.theme.Theme
 
 @Composable
 fun CreateCollectionContent(
@@ -14,11 +18,19 @@ fun CreateCollectionContent(
     leadingIcon : Int,
     hintText : String,
     value : String,
+    title : String,
     onValueChange : (String) -> Unit
 ){
     Column (
         modifier = modifier.fillMaxWidth()
     ) {
+
+        Text(
+            text = title,
+            style = Theme.textStyle.body.md.regular,
+            color = Theme.color.shade.secondary,
+            modifier = Modifier.padding(bottom = 8.dp)
+        )
 
         CollectionTextField(
             leadingIcon = leadingIcon,
@@ -40,6 +52,7 @@ fun CreateCollectionContentPreview(){
         leadingIcon = R.drawable.outline_folder,
         hintText = "e.g. My Watchlist",
         value = "",
+        title = "Collection Name",
         onValueChange = {}
     ) 
 }
