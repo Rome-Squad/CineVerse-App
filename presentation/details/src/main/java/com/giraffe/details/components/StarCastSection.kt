9d3fe3482579d4e3df9cc3f17244a1ec7ce56406
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-07-01 11:54:44 +0300

    fix image scale to crop

--- presentation/details/src/main/java/com/giraffe/details/components/StarCastSection.kt
+++ presentation/details/src/main/java/com/giraffe/details/components/StarCastSection.kt
@@ -22,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -99,6 +100,7 @@ fun CastCard(
             Image(
                 painter = actorImage,
                 contentDescription = "$actorName image",
+                contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .size(64.dp)
                     .clip(
