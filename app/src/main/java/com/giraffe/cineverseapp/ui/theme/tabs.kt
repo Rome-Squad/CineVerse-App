a908be21e937a9a9a2311e69ac695d2fe0fb66db
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-07-01 17:03:49 +0300

    feat: add customizable tab component with indicator and preview

---
+++ app/src/main/java/com/giraffe/cineverseapp/ui/theme/tabs.kt
@@ -0,0 +1,91 @@
+package com.giraffe.cineverseapp.ui.theme
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.giraffe.designsystem.theme.CineVerseTheme
+import com.giraffe.designsystem.theme.Theme
+
+
+@Composable
+fun Tabs(
+    titles: List<String>,
+    selectedTabIndex: Int,
+    onTabSelected: (index: Int) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(modifier = modifier) {
+        TabRow(
+            selectedTabIndex = selectedTabIndex,
+            modifier = Modifier.padding(start = 16.dp, end = 16.dp),
+            containerColor = Color.Transparent,
+            indicator = { tabPositions ->
+                val currentTabPosition = tabPositions[selectedTabIndex]
+                Box(
+                    Modifier
+                        .tabIndicatorOffset(currentTabPosition)
+                        .height(2.dp)
+                        .width(currentTabPosition.width)
+                        .clip(RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp))
+                        .background(Theme.color.brand.primary)
+                )
+            },
+            divider = {}
+        ) {
+            titles.forEachIndexed { index, title ->
+                Tab(
+                    selected = selectedTabIndex == index,
+                    onClick = { onTabSelected(index) },
+                    text = {
+                        Text(
+                            text = title,
+                            color = if (selectedTabIndex == index) Theme.color.brand.primary else Theme.color.shade.tertiary,
+                            style = Theme.textStyle.body.md.medium,
+                        )
+                    }
+                )
+            }
+        }
+        Box(
+            Modifier
+                .fillMaxWidth()
+                .height(1.dp)
+                .clip(RoundedCornerShape(topStart = 8.dp, topEnd = 8.dp))
+                .background(Theme.color.stroke.primary)
+        )
+    }
+}
+
+@Preview(showBackground = true, backgroundColor = 0xFFF7F7F7)
+@Composable
+fun CustomTabsPreview() {
+    CineVerseTheme(isDarkTheme = false) {
+        var selectedIndex by remember { mutableStateOf(0) }
+        val tabTitles = listOf("Home", "Profile", "Settings")
+
+        Tabs(
+            titles = tabTitles,
+            selectedTabIndex = selectedIndex,
+            onTabSelected = { selectedIndex = it }
+        )
+    }
+}
