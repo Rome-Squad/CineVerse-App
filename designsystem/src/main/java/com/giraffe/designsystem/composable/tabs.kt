30fdc1d5d522c12c06273574ecec255d39487fbd
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-07-01 17:24:28 +0300

    refactor: rename package for tab theme to align with design system

--- designsystem/src/main/java/com/giraffe/designsystem/composable/tabs.kt
+++ designsystem/src/main/java/com/giraffe/designsystem/composable/tabs.kt
@@ -1,4 +1,4 @@
-package com.giraffe.cineverseapp.ui.theme
+package com.giraffe.designsystem.composable
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
