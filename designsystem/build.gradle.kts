c6f7124d6c2dcfc09e127bc26b562f385d0a9c8b
bilalazzam <blalazam065@gmail.com>
2025-07-03 13:26:18 +0300

    add image viewer dependencies

--- designsystem/build.gradle.kts
+++ designsystem/build.gradle.kts
@@ -37,6 +37,8 @@ android {
 }
 
 dependencies {
+    implementation(project(":imageviewer"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
