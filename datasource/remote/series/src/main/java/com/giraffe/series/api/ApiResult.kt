ad2cb65657a4167f3c69dc3c8a9f0084d8ffbd53
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-07-08 23:13:12 +0300

    Remove gradle.properties from Git tracking

--- datasource/remote/series/src/main/java/com/giraffe/series/api/ApiResult.kt
+++
@@ -1,6 +0,0 @@
-package com.giraffe.series.api
-
-sealed class ApiResult<out T> {
-    data class Success<T>(val data: T, val statusCode: Int = 200) : ApiResult<T>()
-    data class Error(val exception: Throwable, val message: String = "", val statusCode: Int = 0) : ApiResult<Nothing>()
-}
