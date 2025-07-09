4b94bdcd3a43e9f71838f344860b06480950111e
bilalazzam <blalazam065@gmail.com>
2025-07-09 16:10:42 +0300

    remove test code from main activity

--- app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
+++ app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
@@ -1,35 +1,16 @@
 package com.giraffe.cineverseapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
-import androidx.lifecycle.lifecycleScope
 import com.giraffe.designsystem.theme.CineVerseTheme
-import com.giraffe.designsystem.theme.Theme
-import com.giraffe.explore.datasource.remote.RemoteExploreDataSource
-import com.giraffe.explore.usecase.GetSearchKeywordsUseCase
-import kotlinx.coroutines.launch
-import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
 
-    private val getSearchKeywordsUseCase: GetSearchKeywordsUseCase by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,16 +18,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             CineVerseTheme {
 
-                LaunchedEffect(Unit) {
-                    lifecycleScope.launch {
-                        try {
-                            val result = getSearchKeywordsUseCase("batman")
-                            Log.d("TestSearchKeywords", "Result: $result")
-                        } catch (e: Exception) {
-                            Log.e("TestSearchKeywords", "Error: ${e.message}", e)
-                        }
-                    }
-                }
+
 
             }
         }
