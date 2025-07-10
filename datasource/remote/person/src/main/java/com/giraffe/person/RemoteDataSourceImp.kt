c32a8885cb5517dc8cd090d6f760c605b784f3ab
Mohannad Ahmed <mohannad199344@gmail.com>
2025-07-10 03:55:38 +0300

    feat: add RequestBuilder for API requests and handle error responses

--- datasource/remote/person/src/main/java/com/giraffe/person/RemoteDataSourceImp.kt
+++ datasource/remote/person/src/main/java/com/giraffe/person/RemoteDataSourceImp.kt
@@ -1,47 +1,31 @@
 package com.giraffe.person
 
 import com.giraffe.person.response.SearchPersonResponse
+import com.giraffe.person.util.RequestBuilder
 import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.request.get
-import io.ktor.client.request.headers
-import io.ktor.client.statement.HttpResponse
-import kotlinx.coroutines.ensureActive
-import kotlin.coroutines.coroutineContext
 
 class RemoteDataSourceImp(
     private val client: HttpClient
 ) : RemoteDataSource {
     override suspend fun searchByName(personName: String): SearchPersonResponse {
-        return safeCall<SearchPersonResponse> {
-            client.get(BASE_URL) {
-                url {
-                    parameters.append("query", personName)
-                    parameters.append("include_adult", "false")
-                    parameters.append("language", "en-US")
-                    parameters.append("page", "1")
-                }
-                headers {
-                    append("Authorization", "Bearer $API_KEY")
-                }
-            }
-        }
-    }
-
-    suspend inline fun <reified T> safeCall(execute: () -> HttpResponse): T {
-        val response = try {
-            execute()
-        } catch (exception: Exception) {
-            coroutineContext.ensureActive()
-            throw exception
-        }
-        return response.body<T>()
+        return RequestBuilder(client)
+            .get(
+                endpoint = SEARCH_PERSON_END_POINT,
+                params = mapOf(
+                    QUERY to personName,
+                    INCLUDE_ADULT to "false",
+                    LANGUAGE to "en-US",
+                    PAGE to "1"
+                )
+            )
     }
 
     companion object {
-        private const val API_KEY =
-            "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjNDdkOTgxNjUxZWNmNTMxYmI0ZGRkNTMyYTE5ZDhhZSIsIm5iZiI6MTY4ODU0NjY1OC40NTUwMDAyLCJzdWIiOiI2NGE1MmQ2MjE1OGM4NTAwZmZiYTU1YWMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.vJCN3YBp6HcS1d2zHyoaDMtXpaxu6-kaubtn_G4Z8bQ"
-        private const val BASE_URL = "https://api.themoviedb.org/3/search/person"
+        private const val SEARCH_PERSON_END_POINT = "search/person"
+        private const val QUERY = "query"
+        private const val INCLUDE_ADULT = "include_adult"
+        private const val LANGUAGE = "language"
+        private const val PAGE = "page"
     }
 }
 
