af15207bd947bbbf97674202aa4b6bdf4842eda7
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-07-10 14:25:39 +0300

    create get_movies_by_genre_use_case and its test

---
+++ domain/movies/src/test/kotlin/com/giraffe/movies/usecase/GetMovieGenresUseCaseTest.kt
@@ -0,0 +1,39 @@
+package com.giraffe.movies.usecase
+import com.giraffe.movies.entity.MovieGenre
+import com.giraffe.movies.repository.MoviesRepository
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class GetMovieGenresUseCaseTest {
+
+    private lateinit var repository: MoviesRepository
+    private lateinit var useCase: GetMovieGenresUseCase
+
+    @BeforeEach
+    fun setUp() {
+        repository = mockk()
+        useCase = GetMovieGenresUseCase(repository)
+    }
+
+    @Test
+    fun `should return list of movie genres from repository`() = runTest {
+        // Given
+        val expectedGenres = listOf(
+            MovieGenre(id = 1, title = "Action"),
+            MovieGenre(id = 2, title = "Comedy")
+        )
+        coEvery { repository.getMovieGenres() } returns expectedGenres
+
+        // When
+        val result = useCase()
+
+        // Then
+        coVerify { repository.getMovieGenres() }
+        assertThat(result).isEqualTo(expectedGenres)
+    }
+}
