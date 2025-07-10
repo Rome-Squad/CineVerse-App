1374ab5e0a1afa6cf2b1053aaea283806b275a49
Mohannad Ahmed <mohannad199344@gmail.com>
2025-07-11 02:08:17 +0300

    refactor: remove API_KEY configuration from build.gradle.kts and clean up network module imports

--- app/src/main/java/com/giraffe/cineverseapp/di/networkModule.kt
+++ app/src/main/java/com/giraffe/cineverseapp/di/networkModule.kt
@@ -4,7 +4,6 @@ import com.giraffe.cineverseapp.BuildConfig
 import com.giraffe.cineverseapp.data.network.HttpClientFactory
 import com.giraffe.explore.RemoteExploreDataSourceImpl
 import com.giraffe.explore.datasource.remote.RemoteExploreDataSource
-import com.giraffe.person.BuildConfig
 import com.giraffe.person.PersonRemoteDataSource
 import com.giraffe.person.PersonRemoteDataSourceImp
 import org.koin.core.qualifier.named
