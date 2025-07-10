43fcfb54c4a254173c5bb9453c2202b08db1f824
bilalazzam <blalazam065@gmail.com>
2025-07-10 14:03:42 +0300

    move dto mapping to repo

--- repository/movie/src/main/java/com/giraffe/movie/datasource/remote/MoviesRemoteDataSource.kt
+++ repository/movie/src/main/java/com/giraffe/movie/datasource/remote/MoviesRemoteDataSource.kt
@@ -1,17 +1,17 @@
 package com.giraffe.movie.datasource.remote
 
-import com.giraffe.movies.entity.Movie
+import com.giraffe.movie.datasource.remote.dto.MovieDto
 import com.giraffe.movies.entity.MovieGenre
 
 interface MoviesRemoteDataSource {
 
     suspend fun getMovieById(
         movieId: Int
-    ): Movie
+    ): MovieDto
 
-    suspend fun getMovieByName(movieName: String): List<Movie>
+    suspend fun getMovieByName(movieName: String): List<MovieDto>
 
     suspend fun getMovieGenres(): List<MovieGenre>
 
-    suspend fun getMoviesByGenre(genreId: Int): List<Movie>
+    suspend fun getMoviesByGenre(genreId: Int): List<MovieDto>
 }
