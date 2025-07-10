801c112de64b7b64de8dfbd9a11570f8181c0d08
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-07-10 23:35:24 +0300

    refactor: use base url

--- datasource/remote/movie/src/main/java/com/giraffe/movie/MovieRemoteDataSource.kt
+++ datasource/remote/movie/src/main/java/com/giraffe/movie/MovieRemoteDataSource.kt
@@ -12,11 +12,12 @@ import io.ktor.client.request.headers
 
 class MovieRemoteDataSource(
     val client: HttpClient,
+    val baseUrl: String,
     val accessToken: String,
 ) : MoviesRemoteDataSource {
     override suspend fun getMovieById(movieId: Int): MovieDto {
         return handleRequest<MovieDto> {
-            client.get("$MOVIE_BY_ID_URL$movieId") {
+            client.get(baseUrl + "$MOVIE_BY_ID_URL$movieId") {
                 headers {
                     append("Authorization", "Bearer $accessToken")
                 }
@@ -26,7 +27,7 @@ class MovieRemoteDataSource(
 
     override suspend fun getMovieByName(movieName: String): List<MovieDto> {
         val response: MoviesListResponse = handleRequest<MoviesListResponse> {
-            client.get(MOVIES_BY_NAME_URL) {
+            client.get(baseUrl + MOVIES_BY_NAME_URL) {
                 url {
                     parameters.append("query", movieName)
                 }
@@ -41,34 +42,34 @@ class MovieRemoteDataSource(
     override suspend fun getMovieGenres(): List<GenreDTO> {
         val response =
             handleRequest<GenreResponse> {
-            client.get(GENRES_URL) {
-                headers {
-                    append("Authorization", "Bearer $accessToken")
+                client.get(baseUrl + GENRES_URL) {
+                    headers {
+                        append("Authorization", "Bearer $accessToken")
+                    }
                 }
             }
-        }
         return response.genres
     }
 
     override suspend fun getMoviesByGenre(genreId: Int): List<MovieDto> {
         val response =
             handleRequest<MoviesListResponse> {
-            client.get(MOVIES_BY_GENRE_URL) {
-                url {
-                    parameters.append("with_genres", genreId.toString())
-                }
-                headers {
-                    append("Authorization", "Bearer $accessToken")
+                client.get(baseUrl + MOVIES_BY_GENRE_URL) {
+                    url {
+                        parameters.append("with_genres", genreId.toString())
+                    }
+                    headers {
+                        append("Authorization", "Bearer $accessToken")
+                    }
                 }
             }
-        }
         return response.results
     }
 
     companion object {
-        private const val MOVIE_BY_ID_URL = "https://api.themoviedb.org/3/movie"
-        private const val MOVIES_BY_NAME_URL = "https://api.themoviedb.org/3/search/movie"
-        private const val GENRES_URL = "https://api.themoviedb.org/3/genre/movie/list"
-        private const val MOVIES_BY_GENRE_URL = "https://api.themoviedb.org/3/discover/movie"
+        private const val MOVIE_BY_ID_URL = "movie"
+        private const val MOVIES_BY_NAME_URL = "search/movie"
+        private const val GENRES_URL = "genre/movie/list"
+        private const val MOVIES_BY_GENRE_URL = "discover/movie"
     }
 }
