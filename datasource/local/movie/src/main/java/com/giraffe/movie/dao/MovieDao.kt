59cd53b4f5fdf3b2823de2f0ea37170650ddab36
HendSayed25 <hendsayed853@gmail.com>
2025-07-12 02:10:52 +0300

    edit Local data source and dao

--- datasource/local/movie/src/main/java/com/giraffe/movie/dao/MovieDao.kt
+++ datasource/local/movie/src/main/java/com/giraffe/movie/dao/MovieDao.kt
@@ -18,29 +18,38 @@ interface MovieDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertMovieGenres(movies: List<MovieGenreCacheDto>)
 
-    @Query("SELECT * FROM $MOVIE_TABLE WHERE title MATCH :movieName")
-    suspend fun getMovieByName (movieName : String) : List<MovieCacheDto>
+    @Query("SELECT * FROM $MOVIE_TABLE WHERE title LIKE '%' || :movieName || '%'")
+    suspend fun getMovieByName(movieName: String): List<MovieCacheDto>
 
     @Query("SELECT * FROM $MOVIE_TABLE WHERE id =:movieId")
-    suspend fun getMovieById (movieId : Int) : MovieCacheDto
+    suspend fun getMovieById(movieId: Int): MovieCacheDto
 
     @Query("SELECT * FROM $MOVIE_GENRE_TABLE WHERE id IN (:ids)")
     suspend fun getMovieGenresByIds(ids: List<Int>): List<MovieGenreCacheDto>
 
     @Query("SELECT * FROM $MOVIE_GENRE_TABLE WHERE ID =:id")
-    suspend fun getMovieGenreById(id : Int) : MovieGenreCacheDto
+    suspend fun getMovieGenreById(id: Int): MovieGenreCacheDto
 
     @Query("SELECT * FROM $MOVIE_GENRE_TABLE")
-    suspend fun getMovieGenres() : List<MovieGenreCacheDto>
+    suspend fun getMoviesGenres(): List<MovieGenreCacheDto>
+
+    @Query("SELECT name FROM $MOVIE_GENRE_TABLE WHERE id IN (:genreIds)")
+    suspend fun getMovieGenres(genreIds: List<Int>): List<String>
 
     @Query("SELECT * FROM $MOVIE_TABLE WHERE genresID =:genreId")
-    suspend fun getMoviesByGenre(genreId : Int) : List<MovieCacheDto>
+    suspend fun getMoviesByGenre(genreId: Int): List<MovieCacheDto>
 
     @Query("DELETE FROM $MOVIE_TABLE")
     suspend fun clearMovieCache()
 
     @Query("DELETE FROM $MOVIE_GENRE_TABLE")
     suspend fun clearMovieGenreCache()
+
+    @Query("DELETE FROM $MOVIE_TABLE WHERE isRecent = 1")
+    suspend fun clearRecentlyMovies()
+
+    @Query("SELECT * FROM $MOVIE_TABLE WHERE isRecent = 1")
+    suspend fun getRecentlyMovies(): List<MovieCacheDto>
 
     @Update
     suspend fun updateMovie(movie: MovieCacheDto)
