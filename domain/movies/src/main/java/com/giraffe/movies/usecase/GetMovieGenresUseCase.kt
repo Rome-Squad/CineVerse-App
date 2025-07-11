203b915321f97608924d6a0f00e08b2e1ba2025f
HendSayed25 <hendsayed853@gmail.com>
2025-07-12 01:54:47 +0300

    add get movieGenres use case

--- domain/movies/src/main/java/com/giraffe/movies/usecase/GetMovieGenresUseCase.kt
+++ domain/movies/src/main/java/com/giraffe/movies/usecase/GetMovieGenresUseCase.kt
@@ -1,12 +1,9 @@
 package com.giraffe.movies.usecase
 
-import com.giraffe.movies.entity.MovieGenre
 import com.giraffe.movies.repository.MoviesRepository
 
-class GetMovieGenresUseCase(
+class GetMovieGenresUseCase (
     private val repository: MoviesRepository
-) {
-    suspend operator fun invoke(): List<MovieGenre> {
-        return repository.getMovieGenres()
-    }
+){
+    suspend operator fun invoke(genreIDs : List<Int>) : List<String> = repository.getMovieGenres(genreIDs)
 }
