555cd0f30d8a5556041d378ad1ba6fcacca19d3a
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-07-11 18:37:18 +0300

    Refactor: Remove default parameters from SeriesRemoteDataSource
    This commit simplifies the `SeriesRemoteDataSource` interface by removing default parameter values for `language`, `includeAdult`, `sortBy`, and `includeNullFirstAirDates` from its methods.
    
    The corresponding `TmdbSeriesApiRemoteDataSource` implementation has been updated to reflect these changes.
    
    Additionally, tests in `TmdbSeriesApiRemoteDataSourceTest` have been adjusted to align with the modified interface.

--- repository/series/src/main/java/com/giraffe/series/datasource/remote/SeriesRemoteDataSource.kt
+++ repository/series/src/main/java/com/giraffe/series/datasource/remote/SeriesRemoteDataSource.kt
@@ -4,7 +4,7 @@ import com.giraffe.series.model.GenreDto
 import com.giraffe.series.model.SeriesDto
 
 interface SeriesRemoteDataSource {
-    suspend fun getSeriesByName(name: String, language: String = "en-US", includeAdult: Boolean = false, page: Int = 1): List<SeriesDto>
-    suspend fun getSeriesByGenreId(genreId: Int, language: String = "en-US", includeAdult: Boolean = false, page: Int = 1, sortBy: String = "popularity.desc", includeNullFirstAirDates: Boolean = false): List<SeriesDto>
-    suspend fun getGenres(language: String = "en-US"): List<GenreDto>
+    suspend fun getSeriesByName(name: String, page: Int = 1): List<SeriesDto>
+    suspend fun getSeriesByGenreId(genreId: Int, page: Int = 1): List<SeriesDto>
+    suspend fun getGenres(): List<GenreDto>
 }
