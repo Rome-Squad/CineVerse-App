d8bc288644cacb68655d8110cd8b26bf698d2164
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-07-11 18:20:21 +0300

    replace material3 info section item components with custom implementations.

--- designsystem/src/main/java/com/giraffe/designsystem/composable/InfoSection.kt
+++ designsystem/src/main/java/com/giraffe/designsystem/composable/InfoSection.kt
@@ -3,11 +3,11 @@ package com.giraffe.designsystem.composable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.giraffe.designsystem.composable.custom.Text
 import com.giraffe.designsystem.theme.CineVerseTheme
 import com.giraffe.designsystem.theme.Theme
 
