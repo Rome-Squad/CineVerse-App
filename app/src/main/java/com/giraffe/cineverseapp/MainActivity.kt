c5f1ac443ac8b14a40d5ab700a76bb46d1f822ee
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-07-11 15:09:48 +0300

    Remove commented-out code and unused imports
    This commit removes unused code that was commented out in `TmdbSeriesApiRemoteDataSourceTest.kt` and `MainActivity.kt`. It also removes an unused import from `MainActivity.kt`.

--- app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
+++ app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
@@ -1,26 +1,20 @@
 package com.giraffe.cineverseapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.LaunchedEffect
 import com.giraffe.designsystem.theme.CineVerseTheme
-import com.giraffe.explore.usecase.GetSearchKeywordsUseCase
-import org.koin.android.ext.android.inject
 
 class MainActivity : ComponentActivity() {
 
-val getKeywords: GetSearchKeywordsUseCase by inject()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CineVerseTheme {
                 LaunchedEffect(Unit) {
-//                    val keywords = getKeywords.execute("Batman")
-//                    Log.d("TAG", "onCreate: $keywords")
                 }
             }
         }
