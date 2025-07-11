8fe28a117d88add29904fb14a1e9042b2795c152
bilalazzam <blalazam065@gmail.com>
2025-07-11 10:48:57 +0300

    fix di issues

--- app/src/main/java/com/giraffe/cineverseapp/di/repositoryModule.kt
+++ app/src/main/java/com/giraffe/cineverseapp/di/repositoryModule.kt
@@ -4,6 +4,8 @@ import com.giraffe.explore.ExploreRepositoryImpl
 import com.giraffe.explore.repository.ExploreRepository
 import com.giraffe.movie.MoviesRepositoryImpl
 import com.giraffe.movies.repository.MoviesRepository
+import com.giraffe.person.PersonRepositoryImpl
+import com.giraffe.person.repository.PersonRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
