4f0f48f1e29afc0d411a3d9d4b88859a3cd7b6f0
bilalazzam <blalazam065@gmail.com>
2025-07-11 09:58:07 +0300

    fix room issue by adding converters

--- app/src/main/java/com/giraffe/cineverseapp/data/database/CineVerseDatabase.kt
+++ app/src/main/java/com/giraffe/cineverseapp/data/database/CineVerseDatabase.kt
@@ -3,20 +3,23 @@ package com.giraffe.cineverseapp.data.database
 import MovieDao
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.giraffe.explore.dao.ExploreSearchKeywordDao
 import com.giraffe.explore.model.SearchKeywordCacheDto
+import com.giraffe.movie.converter.Converters
 import com.giraffe.movie.dao.MoviesSearchHistoryDao
 import com.giraffe.movie.datasource.local.cacheDto.MovieCacheDto
 import com.giraffe.movie.datasource.local.cacheDto.MovieGenreCacheDto
 
 @Database(
     entities = [
-        MovieCacheDto::class,
         SearchKeywordCacheDto::class,
+        MovieCacheDto::class,
         MovieGenreCacheDto::class,
     ],
     version = 1
 )
+@TypeConverters(Converters::class)
 abstract class CineVerseDatabase : RoomDatabase() {
     abstract fun movieDao(): MovieDao
     abstract fun moviesSearchHistoryDao(): MoviesSearchHistoryDao
