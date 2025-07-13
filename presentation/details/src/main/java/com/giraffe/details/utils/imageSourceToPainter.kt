7b1089ee9aee03b92f7d24a568989b6c5625af38
Mohamed Ashraf <ma1261327@gmail.com>
2025-07-13 18:07:34 +0300

    add image mapper

---
+++ presentation/details/src/main/java/com/giraffe/details/utils/imageSourceToPainter.kt
@@ -0,0 +1,17 @@
+package com.giraffe.details.utils
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import coil3.compose.rememberAsyncImagePainter
+
+@Composable
+fun imageSourceToPainter(
+    imageSource: Any?
+): Painter {
+    return when (imageSource) {
+        is String -> rememberAsyncImagePainter(model = imageSource) // URL
+        is Int -> painterResource(id = imageSource) // drawable resource
+        else -> throw IllegalArgumentException("Unsupported image source: $imageSource")
+    }
+}
