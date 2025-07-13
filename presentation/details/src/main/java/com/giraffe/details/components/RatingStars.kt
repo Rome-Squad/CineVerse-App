6f15b6744f59db188f164bd5badbbddde317ce69
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-07-14 00:01:41 +0300

    add color animation to rating stars

--- presentation/details/src/main/java/com/giraffe/details/components/RatingStars.kt
+++ presentation/details/src/main/java/com/giraffe/details/components/RatingStars.kt
@@ -1,6 +1,7 @@
 package com.giraffe.details.components
 
 
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -13,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,21 +44,23 @@ fun RatingStars(
         for (i in 0 until 5) {
 
             val isSelected = i < rate
+            val iconColor: Color by animateColorAsState(
+                if (isSelected) Theme.color.additional.primary.yellow
+                else Theme.color.shade.tertiary
+            )
             Icon(
                 painter = painterResource(
 
-                id = if (isSelected) R.drawable.bold_star
-                else R.drawable.outline_star
+                    id = if (isSelected) R.drawable.bold_star
+                    else R.drawable.outline_star
 
-            ),
-                tint = if (isSelected) Theme.color.additional.primary.yellow
-                else Theme.color.shade.tertiary,
+                ),
+                tint = iconColor,
 
                 contentDescription = stringResource(R.string.rate, rate),
                 modifier = Modifier
                     .size(starSize)
                     .then(
-
                         if (onRateClickEnabled) Modifier.clickable { onRateClick(i + 1) }
                         else Modifier
 
@@ -67,7 +71,6 @@ fun RatingStars(
         }
 
     }
-
 }
 
 
