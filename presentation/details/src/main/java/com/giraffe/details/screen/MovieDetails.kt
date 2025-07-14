c498cea3fd99198c979cca6839d2790293fc246f
HendSayed25 <hendsayed853@gmail.com>
2025-07-14 19:22:01 +0300

    edit some design in movie details screen

--- presentation/details/src/main/java/com/giraffe/details/screen/MovieDetails.kt
+++ presentation/details/src/main/java/com/giraffe/details/screen/MovieDetails.kt
@@ -11,10 +11,10 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.giraffe.designsystem.R
 import com.giraffe.designsystem.composable.AppBar
 import com.giraffe.designsystem.composable.InfoSection
 import com.giraffe.designsystem.composable.MoviesListSection
@@ -27,56 +27,42 @@ import com.giraffe.details.components.ReviewCard
 import com.giraffe.details.components.StaffInfoSection
 import com.giraffe.details.components.StaffMember
 import com.giraffe.details.components.StarCastSection
-import com.giraffe.details.components.sampleCastList
 import com.giraffe.details.utils.getCurrentLocalDate
 import com.giraffe.details.utils.imageSourceToPainter
-
-val movies = listOf(
-    Poster(
-        id = 1,
-        name = "The Flash",
-        imageUri = "https://m.media-amazon.com/images/M/MV5BZDU4MGExZGEtMWRlMC00NjRhLThhZGQtMGIxMDFlNjE5MWVlXkEyXkFqcGc@._V1_QL75_UX169_.jpg",
-        rating = 7.5f,
-    ),
-    Poster(
-        id = 2,
-        name = "The Flash",
-        imageUri = "https://m.media-amazon.com/images/M/MV5BZDU4MGExZGEtMWRlMC00NjRhLThhZGQtMGIxMDFlNjE5MWVlXkEyXkFqcGc@._V1_QL75_UX169_.jpg",
-        rating = 7.5f,
-    ),
-    Poster(
-        id = 3,
-        name = "The Flash",
-        imageUri = "https://m.media-amazon.com/images/M/MV5BZDU4MGExZGEtMWRlMC00NjRhLThhZGQtMGIxMDFlNjE5MWVlXkEyXkFqcGc@._V1_QL75_UX169_.jpg",
-        rating = 7.5f,
-    ),
-    Poster(
-        id = 4,
-        name = "The Flash",
-        imageUri = "https://m.media-amazon.com/images/M/MV5BZDU4MGExZGEtMWRlMC00NjRhLThhZGQtMGIxMDFlNjE5MWVlXkEyXkFqcGc@._V1_QL75_UX169_.jpg",
-        rating = 7.5f,
-    )
-)
+import com.giraffe.details.R
+import com.giraffe.details.components.CastMember
 
 @Composable
 fun MovieDetails(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    moviesList: List<Poster>,
+    staffList : List<StaffMember>,
+    castMembersList : List<CastMember>
 ) {
 
     MovieDetailsContent(
         modifier = modifier,
-        onShowMoreReviewsClick = {}
+        onShowMoreReviewsClick = {},
+        moviesList = moviesList,
+        staffList = staffList,
+        castMembersList = castMembersList
     )
 }
 
 @Composable
 private fun MovieDetailsContent(
     modifier: Modifier,
-    onShowMoreReviewsClick : ()->Unit
+    onShowMoreReviewsClick : ()->Unit,
+    moviesList : List<Poster>,
+    staffList : List<StaffMember>,
+    castMembersList : List<CastMember>
+
 ) {
 
     LazyColumn (
-        modifier = modifier.background(Theme.color.background.screen).padding(vertical = 20.dp),
+        modifier = modifier
+            .background(Theme.color.background.screen)
+            .padding(vertical = 20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ){
         item {
@@ -84,6 +70,7 @@ private fun MovieDetailsContent(
                 showBackButton = true, modifier = Modifier.padding(16.dp)
             )
         }
+        // the movie details will changed by viewmodel
         item {
             MainMovieOrSeriesDetails(
                 modifier = Modifier.padding(start = 16.dp, end = 16.dp),
@@ -100,60 +87,30 @@ private fun MovieDetailsContent(
 
             InfoSection(
                 modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
-                title = "Storyline",
+                title = stringResource(R.string.storyline),
                 description = "Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.",
             )
         }
         item {
                 StarCastSection(
-                    title = "Star Cast",
+                    title = stringResource(R.string.star_cast),
                     onShowMoreClick = {},
-                    castList = sampleCastList(),
-                    modifier = Modifier.padding(start = 16.dp)
+                    castList = castMembersList,
                 )
             }
         item {
             StaffInfoSection(
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
-                title = "Behind the Scenes",
+                modifier = Modifier.padding( horizontal = 16.dp,vertical = 24.dp),
+                title = stringResource(R.string.behind_the_scenes),
                 onShowMoreClick = {},
-                staffList = listOf(
-                    StaffMember(name = "John Doe", role = "Director"),
-                    StaffMember(name = "Christopher Nolan", role = "Director, Screenplay, Story"),
-                    StaffMember(name = "Mike Johnson", role = "Writer")
-                )
+                staffList = staffList
             )
         }
         item {
             MoviesListSection(
-                title = "You Might Also Like",
-                endText = "Show More",
-                movies = listOf(
-                    Poster(
-                        id = 1,
-                        name = "The Flash",
-                        imageUri = "",
-                        rating = 7.5f,
-                    ),
-                    Poster(
-                        id = 2,
-                        name = "The Flash",
-                        imageUri = "",
-                        rating = 7.5f,
-                    ),
-                    Poster(
-                        id = 3,
-                        name = "The Flash",
-                        imageUri = "",
-                        rating = 7.5f,
-                    ),
-                    Poster(
-                        id = 4,
-                        name = "The Flash",
-                        imageUri = "",
-                        rating = 7.5f,
-                    ),
-                ),
+                title = stringResource(R.string.you_might_also_like),
+                endText = stringResource(R.string.show_more),
+                movies = moviesList,
                 onClickEndText = {},
                 onClickPoster = {}
             )
@@ -162,23 +119,25 @@ private fun MovieDetailsContent(
         item {
             RatingSection(
                 modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp)
-            )
+            ) // when click to arrow show bottom sheet or all card ?
         }
 
         item{
             Row(
-                modifier = Modifier.fillMaxWidth().padding(horizontal = 16.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 16.dp),
                 horizontalArrangement = Arrangement.SpaceBetween,
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Text(
-                    text = "Top Reviews",
+                    text = stringResource(R.string.top_reviews),
                     color = Theme.color.shade.primary,
                     style = Theme.textStyle.title.sm,
                 )
 
                 Text(
-                    text = stringResource(com.giraffe.details.R.string.show_more),
+                    text = stringResource(R.string.show_more),
                     color = Theme.color.brand.primary,
                     modifier = Modifier
                         .padding(start = 12.dp)
@@ -187,6 +146,8 @@ private fun MovieDetailsContent(
                 )
             }
         }
+
+        //this data in review card will changed
 
         item{
             ReviewCard(
@@ -230,6 +191,50 @@ private fun MovieDetailsPreview() {
     CineVerseTheme(
         isDarkTheme = false
     ) {
-        MovieDetails()
+        val movies =  listOf(
+            Poster(
+                id = 1,
+                name = "The Flash",
+                imageUri = "",
+                rating = 7.5f,
+            ),
+            Poster(
+                id = 2,
+                name = "The Flash",
+                imageUri = "",
+                rating = 7.5f,
+            ),
+            Poster(
+                id = 3,
+                name = "The Flash",
+                imageUri = "",
+                rating = 7.5f,
+            ),
+            Poster(
+                id = 4,
+                name = "The Flash",
+                imageUri = "",
+                rating = 7.5f,
+            ),
+        )
+
+        val staffList= listOf(
+            StaffMember(name = "John Doe", role = "Director"),
+            StaffMember(name = "Christopher Nolan", role = "Director, Screenplay, Story"),
+            StaffMember(name = "Mike Johnson", role = "Writer")
+        )
+
+        val castMembers = listOf(
+            CastMember("Robert Downey Jr.", "Iron Man", painterResource(id = com.giraffe.designsystem.R.drawable.reviewer)),
+            CastMember("Chris Evans", "Captain America", painterResource(id = com.giraffe.designsystem.R.drawable.reviewer)),
+            CastMember("Scarlett Johansson", "Black Widow", painterResource(id = com.giraffe.designsystem.R.drawable.reviewer)),
+            CastMember("Mark Ruffalo", "Hulk", painterResource(id = com.giraffe.designsystem.R.drawable.reviewer))
+        )
+
+        MovieDetails(
+            moviesList = movies,
+            staffList = staffList,
+            castMembersList = castMembers
+        )
     }
 }
