439a42827dc891c90797507afacae80b8641b6b0
HendSayed25 <hendsayed853@gmail.com>
2025-07-15 00:14:42 +0300

    convert from LocalDate To LocalDateTime recording to entity

--- presentation/details/src/main/java/com/giraffe/details/components/ReviewCard.kt
+++ presentation/details/src/main/java/com/giraffe/details/components/ReviewCard.kt
@@ -20,9 +20,8 @@ import com.giraffe.designsystem.R
 import com.giraffe.designsystem.theme.CineVerseTheme
 import com.giraffe.designsystem.theme.Theme
 import com.giraffe.details.utils.formatAsMonthDayYear
-import com.giraffe.details.utils.getCurrentLocalDate
-import com.giraffe.details.utils.imageSourceToPainter
-import kotlinx.datetime.LocalDate
+import com.giraffe.details.utils.getCurrentLocalDateTime
+import kotlinx.datetime.LocalDateTime
 
 
 @Composable
@@ -30,7 +29,7 @@ fun ReviewCard(
     modifier: Modifier = Modifier,
     rate: Int,
     reviewText: String,
-    reviewDate: LocalDate = getCurrentLocalDate(),
+    reviewDate: LocalDateTime = getCurrentLocalDateTime(),
     reviewerImageSource: Any?,
     reviewerName: String,
     reviewerUsername: String
@@ -115,7 +114,7 @@ fun PreviewReviewCard() {
             modifier = Modifier,
             rate = 5,
             reviewText = "very good",
-            reviewDate = getCurrentLocalDate(),
+            reviewDate = getCurrentLocalDateTime(),
             reviewerImageSource = R.drawable.reviewer,
             reviewerName = "Hend",
             reviewerUsername = "Hend sayed",
@@ -136,7 +135,7 @@ fun PreviewReviewCardDark() {
             modifier = Modifier,
             rate = 5,
             reviewText = "very good",
-            reviewDate = getCurrentLocalDate(),
+            reviewDate = getCurrentLocalDateTime(),
             reviewerImageSource = R.drawable.reviewer,
             reviewerName = "Hend",
             reviewerUsername = "Hend sayed"
