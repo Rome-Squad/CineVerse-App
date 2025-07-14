0a176a88c13eabaebb9e7514d13074301960b72f
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-07-15 01:28:25 +0300

    add movie rating use case test

---
+++ domain/movies/src/test/kotlin/com/giraffe/movies/usecase/AddMovieRatingUseCaseTest.kt
@@ -0,0 +1,34 @@
+package com.giraffe.movies.usecase
+
+import com.giraffe.movies.repository.MoviesRepository
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import kotlin.test.Test
+
+class AddMovieRatingUseCaseTest {
+
+    private lateinit var repository: MoviesRepository
+    private lateinit var addMovieRatingUseCase: AddMovieRatingUseCase
+
+    @BeforeEach
+    fun setup() {
+        repository = mockk()
+        addMovieRatingUseCase = AddMovieRatingUseCase(repository)
+    }
+
+    @Test
+    fun `invoke should call addRating on repository with correct data`() = runTest {
+        // given
+        coEvery { repository.addRating(any(), any()) } returns Unit
+
+        // when
+        addMovieRatingUseCase(movieId = 1, ratingValue = 8.5f)
+
+        // then
+        coVerify(exactly = 1) { repository.addRating(1, 8.5f) }
+    }
+}
