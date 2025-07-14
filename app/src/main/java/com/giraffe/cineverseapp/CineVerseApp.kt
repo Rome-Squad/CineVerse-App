21cdce75f71bcdbc006c92923474f857ff7bf90f
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-07-14 16:33:16 +0300

    create guest session in app if not exists to be created once

--- app/src/main/java/com/giraffe/cineverseapp/CineVerseApp.kt
+++ app/src/main/java/com/giraffe/cineverseapp/CineVerseApp.kt
@@ -1,6 +1,7 @@
 package com.giraffe.cineverseapp
 
 import android.app.Application
+import android.util.Log
 import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
@@ -11,12 +12,24 @@ import com.giraffe.cineverseapp.di.repositoryModule
 import com.giraffe.cineverseapp.di.useCaseModule
 import com.giraffe.cineverseapp.di.viewModelModule
 import com.giraffe.cineverseapp.worker.CacheCleanupWorker
+import com.giraffe.movie.datasource.remote.SessionRepository
+import com.giraffe.movies.usecase.CreateGuestSessionUseCase
 import com.giraffe.series.di.seriesRemoteDataModule
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 import java.util.concurrent.TimeUnit
 
 class CineVerseApp : Application() {
+
+    private val applicationScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+    private val createGuestSessionUseCase: CreateGuestSessionUseCase by inject()
+    private val sessionRepository: SessionRepository by inject()
     override fun onCreate() {
         super.onCreate()
         startKoin {
@@ -32,6 +45,26 @@ class CineVerseApp : Application() {
             )
         }
 
+        createGuestSessionIfNotExists()
+
+        setupCacheCleanupWorker()
+    }
+    private fun createGuestSessionIfNotExists() {
+        applicationScope.launch {
+            if (sessionRepository.getGuestSessionId() == null) {
+
+                try {
+                    val newGuestSessionId = createGuestSessionUseCase()
+
+                    sessionRepository.saveGuestSessionId(newGuestSessionId)
+                } catch (e: Exception) {
+                    Log.e("CineVerseApp", "Failed to create guest session", e)
+                }
+            }
+        }
+    }
+
+    private fun setupCacheCleanupWorker() {
         val workRequest = PeriodicWorkRequestBuilder<CacheCleanupWorker>(
             1, TimeUnit.HOURS
         ).build()
