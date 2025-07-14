05c7c2bdf5320f13eac5f440228691f09673c6ae
Mohannad Ahmed <mohannad199344@gmail.com>
2025-07-15 01:00:36 +0300

    Remove SearchViewModel from Koin dependency injection.

--- app/src/main/java/com/giraffe/cineverseapp/di/viewModelModule.kt
+++ app/src/main/java/com/giraffe/cineverseapp/di/viewModelModule.kt
@@ -1,14 +1,11 @@
 package com.giraffe.cineverseapp.di
 
 import com.giraffe.explore.ExploreViewModel
-import org.koin.core.module.dsl.viewModel
 import com.giraffe.details.screens.castDetails.CastDetailsViewModel
-import com.giraffe.explore.screen.SearchViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val viewModelModule = module {
     viewModelOf(::ExploreViewModel)
-    viewModelOf(::SearchViewModel)
     viewModelOf(::CastDetailsViewModel)
 }
