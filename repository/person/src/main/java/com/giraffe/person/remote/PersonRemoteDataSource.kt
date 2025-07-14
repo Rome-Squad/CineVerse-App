25d9cc4eef0335e2479aa9cd1ec9a07eaab348d7
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-07-14 22:13:05 +0300

    make the api call for person details

--- repository/person/src/main/java/com/giraffe/person/remote/PersonRemoteDataSource.kt
+++ repository/person/src/main/java/com/giraffe/person/remote/PersonRemoteDataSource.kt
@@ -2,9 +2,19 @@ package com.giraffe.person.remote
 
 import com.giraffe.person.remote.response.SearchPersonResponse
 import com.giraffe.person.remote.response.CreditsResponse
+import com.giraffe.person.remote.response.PersonDetailsResponse
+import com.giraffe.person.remote.response.PersonMovieCastItemResponse
+import com.giraffe.person.remote.response.PersonMovieCreditsResponse
+import com.giraffe.person.remote.response.PersonProfileImageResponse
+import com.giraffe.person.remote.response.PersonTvCastItemResponse
 
 interface PersonRemoteDataSource {
     suspend fun searchByName(personName: String): SearchPersonResponse
     suspend fun getCreditsBySeriesId(seriesId: Int): CreditsResponse
     suspend fun getCreditsByMovieId(movieId: Int): CreditsResponse
+    suspend fun getPersonDetails(personId: Int): PersonDetailsResponse
+    suspend fun getPersonImages(personId: Int): PersonProfileImageResponse
+    suspend fun getPersonMovieCredits(personId: Int): PersonMovieCreditsResponse
+
+    suspend fun getPersonTvCredits(personId: Int): PersonTvCastItemResponse
 }
