01584a28a454c58308e80781a927df270839fc32
HendSayed25 <hendsayed853@gmail.com>
2025-07-15 19:01:44 +0300

    connect view model with movie details screen

--- presentation/details/src/main/java/com/giraffe/details/screen/MovieDetails.kt
+++ presentation/details/src/main/java/com/giraffe/details/screen/MovieDetails.kt
@@ -3,14 +3,19 @@ package com.giraffe.details.screen
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -18,114 +23,124 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giraffe.designsystem.composable.AppBar
+import com.giraffe.designsystem.composable.BaseBottomSheet
 import com.giraffe.designsystem.composable.InfoSection
 import com.giraffe.designsystem.composable.MoviesListSection
+import com.giraffe.designsystem.composable.button_type.PrimaryButton
 import com.giraffe.designsystem.theme.CineVerseTheme
 import com.giraffe.designsystem.theme.Theme
-import com.giraffe.designsystem.uimodel.Poster
+import com.giraffe.details.R
+import com.giraffe.details.components.CollectionItem
 import com.giraffe.details.components.MainMovieOrSeriesDetails
 import com.giraffe.details.components.RatingSection
+import com.giraffe.details.components.RatingSelector
 import com.giraffe.details.components.ReviewCard
 import com.giraffe.details.components.StaffInfoSection
-import com.giraffe.details.components.StaffMember
 import com.giraffe.details.components.StarCastSection
-import com.giraffe.details.utils.getCurrentLocalDate
+import com.giraffe.details.screens.moviedetails.MovieDetailsScreenState
+import com.giraffe.details.screens.moviedetails.MovieDetailsViewModel
 import com.giraffe.details.utils.imageSourceToPainter
-import com.giraffe.details.R
-import com.giraffe.details.components.CastMember
-import com.giraffe.details.utils.getCurrentLocalDateTime
+import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun MovieDetails(
-    modifier: Modifier = Modifier,
-    moviesList: List<Poster>,
-    staffList : List<StaffMember>,
-    castMembersList : List<CastMember>
+    movieID: Int, modifier: Modifier = Modifier, viewModel: MovieDetailsViewModel = koinViewModel()
 ) {
+    val state = viewModel.state.collectAsState().value
+
+    LaunchedEffect(movieID) {
+        viewModel.loadMovieDetails(movieID)
+    }
 
     MovieDetailsContent(
         modifier = modifier,
-        onShowMoreReviewsClick = {},
-        moviesList = moviesList,
-        staffList = staffList,
-        castMembersList = castMembersList
+        state = state,
+        onShowMoreReviewsClick = viewModel::onShowMoreReviewsClick,
+        onAddToCollectionClick = viewModel::onAddToCollectionClick,
+        onShowMoreMoviesClick = viewModel::onShowMoreMoviesClick,
+        onGiveStarClick = viewModel::onGiveStarsClick,
+        onDismissAddToCollectionBottomSheet = viewModel::onDismissAddToCollectionBottomSheet,
+        onDismissAddRatingBottomSheet = viewModel::onDismissGiveStarsBottomSheet,
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun MovieDetailsContent(
     modifier: Modifier,
-    onShowMoreReviewsClick : ()->Unit,
-    moviesList : List<Poster>,
-    staffList : List<StaffMember>,
-    castMembersList : List<CastMember>
-
+    state: MovieDetailsScreenState,
+    onShowMoreReviewsClick: () -> Unit,
+    onAddToCollectionClick: () -> Unit,
+    onDismissAddToCollectionBottomSheet: () -> Unit,
+    onShowMoreMoviesClick: () -> Unit,
+    onGiveStarClick: () -> Unit,
+    onDismissAddRatingBottomSheet: () -> Unit
 ) {
 
-    LazyColumn (
+    LazyColumn(
         modifier = modifier
             .background(Theme.color.background.screen)
             .padding(vertical = 20.dp),
         horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    ) {
         item {
             AppBar(
                 showBackButton = true, modifier = Modifier.padding(16.dp)
             )
         }
-        // the movie details will changed by viewmodel
         item {
             MainMovieOrSeriesDetails(
+                type = "Movie",
+                poster = state.movie.posterUrl?.imageSourceToPainter()
+                    ?: painterResource(R.drawable.main_poster_test),
+                name = state.movie.title,
+                genres = state.movieGenres,
+                rating = state.movie.rating,
+                duration = state.movie.duration.toString(),
+                releaseDate = state.movie.releaseYear,
                 modifier = Modifier.padding(start = 16.dp, end = 16.dp),
-                poster = R.drawable.main_poster_test.imageSourceToPainter(),
-                name = "The Dark Knight",
-                genres = listOf("Drama", "Action", "Thriller", "Drama", "Action", "Crime"),
-                rating = 5.5f,
-                duration = "2h 32m",
-                releaseDate = "2008, Jul 18",
-                type = "Movie"
+                onAddToCollectionClick = onAddToCollectionClick
             )
         }
         item {
-
             InfoSection(
                 modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
                 title = stringResource(R.string.storyline),
-                description = "Batman raises the stakes in his war on crime. With the help of Lt. Jim Gordon and District Attorney Harvey Dent, Batman sets out to dismantle the remaining criminal organizations that plague the streets. The partnership proves to be effective, but they soon find themselves prey to a reign of chaos unleashed by a rising criminal mastermind known to the terrified citizens of Gotham as the Joker.",
+                description = state.movie.description
             )
         }
         item {
-                StarCastSection(
-                    title = stringResource(R.string.star_cast),
-                    onShowMoreClick = {},
-                    castList = castMembersList,
-                )
-            }
+            state.cast.map { }
+            StarCastSection(
+                title = stringResource(R.string.star_cast),
+                onShowMoreClick = {},
+                castList = state.cast
+            )
+        }
         item {
             StaffInfoSection(
-                modifier = Modifier.padding( horizontal = 16.dp,vertical = 24.dp),
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
                 title = stringResource(R.string.behind_the_scenes),
-                onShowMoreClick = {},
-                staffList = staffList
+                staffList = state.crew
             )
         }
         item {
             MoviesListSection(
                 title = stringResource(R.string.you_might_also_like),
                 endText = stringResource(R.string.show_more),
-                movies = moviesList,
-                onClickEndText = {},
-                onClickPoster = {}
-            )
+                movies = state.recommendedMovies,
+                onClickEndText = onShowMoreMoviesClick,
+                onClickPoster = {})
         }
 
         item {
             RatingSection(
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp)
+                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+                onArrowButtonClick = onGiveStarClick
             ) // when click to arrow show bottom sheet or all card ?
         }
 
-        item{
+        item {
             Row(
                 modifier = Modifier
                     .fillMaxWidth()
@@ -145,52 +160,69 @@ private fun MovieDetailsContent(
                     modifier = Modifier
                         .padding(start = 12.dp)
                         .clickable { onShowMoreReviewsClick() },
-                    style = Theme.textStyle.body.md.medium
+                    style = Theme.textStyle.body.md.medium,
                 )
             }
         }
 
-        //this data in review card will changed
-
-        item{
+        itemsIndexed(state.movieReviews.take(3)) { index, review ->
+            val padding = when (index) {
+                0 -> Modifier.padding(vertical = 12.dp, horizontal = 16.dp)
+                1 -> Modifier.padding(horizontal = 16.dp)
+                2 -> Modifier.padding(top = 12.dp, start = 16.dp, end = 16.dp)
+                else -> Modifier
+            }
             ReviewCard(
-                modifier = Modifier.padding(vertical = 12.dp, horizontal = 16.dp),
-                rate = 5,
-                reviewText = "very good",
-                reviewDate = getCurrentLocalDateTime(),
-                reviewerImageSource = R.drawable.reviewer,
-                reviewerName = "Hend",
-                reviewerUsername = "Hend sayed"
-            )
-        }
-        item{
-            ReviewCard(
-                modifier = Modifier.padding(horizontal = 16.dp),
-                rate = 5,
-                reviewText = "very good",
-                reviewDate = getCurrentLocalDateTime(),
-                reviewerImageSource = R.drawable.reviewer,
-                reviewerName = "Hend",
-                reviewerUsername = "Hend sayed"
-            )
-        }
-        item{
-            ReviewCard(
-                modifier = Modifier.padding(top=12.dp, start = 16.dp,end=16.dp),
-                rate = 5,
-                reviewText = "very good",
-                reviewDate = getCurrentLocalDateTime(),
-                reviewerImageSource = R.drawable.reviewer,
-                reviewerName = "Hend",
-                reviewerUsername = "Hend sayed"
+                modifier = padding,
+                rate = review.author.rating.toInt(),
+                reviewText = review.content,
+                reviewDate = review.createdAt,
+                reviewerImageSource = review.author.avatarImage,
+                reviewerName = review.author.name,
+                reviewerUsername = review.author.username
             )
         }
 
-        item{
+        item {
             Spacer(Modifier.height(30.dp))
         }
     }
+
+    BaseBottomSheet(
+        isVisible = state.isVisibleAddToCollectionBottomSheet,
+        onDismiss = onDismissAddToCollectionBottomSheet,
+        title = "Add to Collection",
+        content = {
+            CollectionItem(
+                text = "My Favorite TV", icon = R.drawable.due_tone_folder
+            )
+            CollectionItem(
+                text = "My WatchList", icon = R.drawable.due_tone_folder
+            )
+            CollectionItem(
+                text = "Cristian Bale Movies", icon = R.drawable.due_tone_folder
+            )
+        },
+    )
+    BaseBottomSheet(
+        isVisible = state.isVisibleGiveStarsBottomSheet,
+        onDismiss = onDismissAddRatingBottomSheet,
+        title = "Rate the Movie",
+        content = {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                RatingSelector()
+                PrimaryButton(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 24.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)
+                        .weight(1f), text = "Add To Rate", enabled = false, onClick = {})
+            }
+        },
+    )
 }
+
 
 @Preview
 @Composable
@@ -198,50 +230,8 @@ private fun MovieDetailsPreview() {
     CineVerseTheme(
         isDarkTheme = false
     ) {
-        val movies =  listOf(
-            Poster(
-                id = 1,
-                name = "The Flash",
-                imageUri = "",
-                rating = 7.5f,
-            ),
-            Poster(
-                id = 2,
-                name = "The Flash",
-                imageUri = "",
-                rating = 7.5f,
-            ),
-            Poster(
-                id = 3,
-                name = "The Flash",
-                imageUri = "",
-                rating = 7.5f,
-            ),
-            Poster(
-                id = 4,
-                name = "The Flash",
-                imageUri = "",
-                rating = 7.5f,
-            ),
-        )
-
-        val staffList= listOf(
-            StaffMember(name = "John Doe", role = "Director"),
-            StaffMember(name = "Christopher Nolan", role = "Director, Screenplay, Story"),
-            StaffMember(name = "Mike Johnson", role = "Writer")
-        )
-
-        val castMembers = listOf(
-            CastMember("Robert Downey Jr.", "Iron Man", painterResource(id = com.giraffe.designsystem.R.drawable.reviewer)),
-            CastMember("Chris Evans", "Captain America", painterResource(id = com.giraffe.designsystem.R.drawable.reviewer)),
-            CastMember("Scarlett Johansson", "Black Widow", painterResource(id = com.giraffe.designsystem.R.drawable.reviewer)),
-            CastMember("Mark Ruffalo", "Hulk", painterResource(id = com.giraffe.designsystem.R.drawable.reviewer))
-        )
-
         MovieDetails(
-            moviesList = movies,
-            staffList = staffList,
-            castMembersList = castMembers
+            movieID = 286,
         )
     }
 }
