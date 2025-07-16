31446a06713be5e10b7767e84381d0b94e21920b
Mohannad Ahmed <mohannad199344@gmail.com>
2025-07-16 12:11:58 +0300

    Refactor: Update exception handling by replacing NetworkException with NoInternetException

--- repository/media/src/main/java/com/giraffe/media/person/PersonRepositoryImpl.kt
+++ repository/media/src/main/java/com/giraffe/media/person/PersonRepositoryImpl.kt
@@ -7,12 +7,10 @@ import com.giraffe.media.person.remote.PersonRemoteDataSource
 import com.giraffe.media.person.remote.dto.CreditsDto
 import com.giraffe.media.person.repository.PersonRepository
 import com.giraffe.media.person.util.toDto
-import com.giraffe.media.person.util.toMovieCredits
 import com.giraffe.media.person.util.toImageList
-import com.giraffe.media.person.util.toPersonMovieCredits
-import com.giraffe.media.person.util.toPersonTvCredits
-import com.giraffe.media.util.SafeCall
+import com.giraffe.media.person.util.toMovieCredits
 import com.giraffe.media.person.util.toTvCredits
+import com.giraffe.media.util.SafeCall
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.async
 import kotlinx.coroutines.withContext
