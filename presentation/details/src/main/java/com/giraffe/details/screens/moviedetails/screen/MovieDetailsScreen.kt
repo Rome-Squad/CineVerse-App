5056a2715042bb0ae7953348c5901239d62a21b5
HendSayed25 <hendsayed853@gmail.com>
2025-07-16 21:45:49 +0300

    Rename MovieDetails to MovieDetailsScreen

--- presentation/details/src/main/java/com/giraffe/details/screens/moviedetails/screen/MovieDetailsScreen.kt
+++ presentation/details/src/main/java/com/giraffe/details/screens/moviedetails/screen/MovieDetailsScreen.kt
@@ -1,4 +1,4 @@
-package com.giraffe.details.screens.moviedetails.screens
+package com.giraffe.details.screens.moviedetails.screen
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
