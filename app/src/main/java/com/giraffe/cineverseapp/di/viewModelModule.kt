ee49edf4d620579c5069bb3a62c6e24937d49d7f
HendSayed25 <hendsayed853@gmail.com>
2025-07-16 15:41:04 +0300

    add details view model in di

--- app/src/main/java/com/giraffe/cineverseapp/di/viewModelModule.kt
+++ app/src/main/java/com/giraffe/cineverseapp/di/viewModelModule.kt
@@ -1,8 +1,8 @@
 package com.giraffe.cineverseapp.di
 
 import com.giraffe.details.screens.castDetails.CastDetailsViewModel
-import com.giraffe.media.explore.ExploreViewModel
 import com.giraffe.details.screens.moviedetails.MovieDetailsViewModel
+import com.giraffe.explore.ExploreViewModel
 import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
