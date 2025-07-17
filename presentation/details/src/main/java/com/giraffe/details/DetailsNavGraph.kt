6cb62194912f1378c199db7e4fa369ca34b2fd62
HendSayed25 <hendsayed853@gmail.com>
2025-07-17 18:24:17 +0300

    Add DetailsNavGraph and its routes

---
+++ presentation/details/src/main/java/com/giraffe/details/DetailsNavGraph.kt
@@ -0,0 +1,22 @@
+package com.giraffe.details
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import com.giraffe.details.screens.moviedetails.screen.MOVIES_ROUTE
+import com.giraffe.details.screens.moviedetails.screen.movieDetailsRoute
+import com.giraffe.details.screens.reviewScreen.REVIEW_LIST_ARG
+import com.giraffe.details.screens.reviewScreen.Review_ROUTE
+import com.giraffe.details.screens.reviewScreen.reviewRoute
+
+
+@Composable
+fun DetailsNavGraph(navController: NavHostController) {
+NavHost(navController = navController, startDestination = "$MOVIES_ROUTE/${268}"){
+        movieDetailsRoute(navController, navigateToReviews = {reviews->
+            navController.currentBackStackEntry?.savedStateHandle?.set(REVIEW_LIST_ARG, reviews)
+            navController.navigate(Review_ROUTE)
+        })
+        reviewRoute(navController)
+    }
+}
