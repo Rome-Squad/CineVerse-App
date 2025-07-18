46aadf4e272baf486f9b0082f9c055771a093582
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-07-19 01:53:41 +0300

    implement navigation to cast details screen

--- app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
+++ app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
@@ -5,24 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import com.giraffe.details.DetailsApi
-//import com.giraffe.details.DetailsApi
-import com.giraffe.details.screens.castDetails.DetailsApiImp
-//import com.giraffe.explore.ExploreApi
-//import com.giraffe.explore.ExploreApi
+import com.giraffe.details.navigation.DetailsApiImp
+import com.giraffe.explore.ExploreApi
 import org.koin.android.ext.android.inject
 
 
 class MainActivity : ComponentActivity() {
-//    private val exploreApi: ExploreApi by inject()
+    private val exploreApi: ExploreApi by inject()
     private val detailsApi: DetailsApi = DetailsApiImp()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-//            exploreApi.CreateSearchScreen { personId ->
-                detailsApi.GetSeriesDetailsContainer(1022)
-//            }
+                detailsApi.GetCastDetailsContainer(1)
         }
     }
 }
