0f09828e9338b020629c02a01524ffed03841c4e
pola george <86931053+poula22@users.noreply.github.com>
2025-07-18 13:32:59 +0300

    - implement series review with pagination

--- presentation/details/src/main/java/com/giraffe/details/screens/seriesRecommendation/SeriesRecommendationViewModel.kt
+++ presentation/details/src/main/java/com/giraffe/details/screens/seriesRecommendation/SeriesRecommendationViewModel.kt
@@ -5,9 +5,12 @@ import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
+import androidx.paging.map
 import com.giraffe.details.base.BasePagingSource
+import com.giraffe.details.models.SeriesUi
 import com.giraffe.media.series.usecase.GetRecommendedSeriesUseCase
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 
 class SeriesRecommendationViewModel(
@@ -18,5 +21,6 @@ class SeriesRecommendationViewModel(
         BasePagingSource { page -> getRecommendedSeries(seriesId, page) }
     }
         .flow
+        .map { it.map(SeriesUi::fromEntity) }
         .stateIn(viewModelScope, SharingStarted.Lazily, PagingData.empty())
 }
