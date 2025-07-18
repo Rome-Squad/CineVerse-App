8de8d34571018c4a4a31fc2ecff0122c7082c59a
HendSayed25 <hendsayed853@gmail.com>
2025-07-18 22:42:35 +0300

    Modify MainActivity to display DetailsNavGraph

--- app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
+++ app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.navigation.compose.rememberNavController
 import com.giraffe.designsystem.theme.CineVerseTheme
+import com.giraffe.details.DetailsNavGraph
 import com.giraffe.explore.nav.ExploreNavGraph
 
 class MainActivity : ComponentActivity() {
@@ -13,7 +15,10 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             CineVerseTheme {
-                ExploreNavGraph()
+                val navController = rememberNavController()
+                DetailsNavGraph(navController = navController)
+
+               // ExploreNavGraph()
             }
         }
     }
