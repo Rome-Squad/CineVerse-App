f16eec8cbd2d9df767b071965c7d2fb95e375baf
HendSayed25 <hendsayed853@gmail.com>
2025-07-18 22:52:16 +0300

    Add Details API and implement its usage

--- app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
+++ app/src/main/java/com/giraffe/cineverseapp/MainActivity.kt
@@ -4,17 +4,29 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.giraffe.designsystem.theme.CineVerseTheme
-import com.giraffe.explore.nav.ExploreNavGraph
+import com.giraffe.details.DetailsApi
+//import com.giraffe.details.DetailsApi
+import com.giraffe.details.screens.castDetails.DetailsApiImp
+//import com.giraffe.explore.ExploreApi
+//import com.giraffe.explore.ExploreApi
+import org.koin.android.ext.android.inject
+
+
 
 class MainActivity : ComponentActivity() {
+//    private val exploreApi: ExploreApi by inject()
+    private val detailsApi: DetailsApi = DetailsApiImp()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CineVerseTheme {
-                ExploreNavGraph()
-            }
+
+//            exploreApi.CreateSearchScreen { personId ->
+            detailsApi.GetSeriesDetailsContainer(1022)
+
+
         }
     }
 }
+
