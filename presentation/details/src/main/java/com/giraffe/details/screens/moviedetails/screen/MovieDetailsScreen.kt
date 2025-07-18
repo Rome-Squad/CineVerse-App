4eec769609562b8c4720bb34b9214c357b6451e8
HendSayed25 <hendsayed853@gmail.com>
2025-07-18 22:41:47 +0300

    Refactored the MovieDetailsScreen to use a Column with vertical scrolling instead of a LazyColumn.

--- presentation/details/src/main/java/com/giraffe/details/screens/moviedetails/screen/MovieDetailsScreen.kt
+++ presentation/details/src/main/java/com/giraffe/details/screens/moviedetails/screen/MovieDetailsScreen.kt
@@ -1,5 +1,6 @@
 package com.giraffe.details.screens.moviedetails.screen
 
+import android.util.Log
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
@@ -7,13 +8,18 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -25,11 +31,15 @@ import androidx.navigation.NavController
 import com.giraffe.designsystem.composable.AppBar
 import com.giraffe.designsystem.composable.InfoSection
 import com.giraffe.designsystem.composable.MoviesListSection
+import com.giraffe.designsystem.composable.SectionTitle
 import com.giraffe.designsystem.composable.custom.Text
 import com.giraffe.designsystem.theme.Theme
 import com.giraffe.details.R
+import com.giraffe.details.components.MainMovieOrSeriesDetails
+import com.giraffe.details.components.MainMovieOrSeriesDetailsAnimatedContent
 import com.giraffe.details.components.RatingSection
 import com.giraffe.details.components.ReviewCard
+import com.giraffe.details.components.SeasonCard
 import com.giraffe.details.components.StaffInfoSection
 import com.giraffe.details.components.StarCastSection
 import com.giraffe.details.models.ReviewUI
@@ -37,7 +47,9 @@ import com.giraffe.details.screens.moviedetails.MovieDetailsEffect
 import com.giraffe.details.screens.moviedetails.MovieDetailsInteractionListener
 import com.giraffe.details.screens.moviedetails.MovieDetailsScreenState
 import com.giraffe.details.screens.moviedetails.MovieDetailsViewModel
+import com.giraffe.details.utils.TypeOfDetailsScreen
 import org.koin.androidx.compose.koinViewModel
+import kotlin.math.min
 
 
 @Composable
@@ -51,6 +63,7 @@ fun MovieDetailsScreen(
     val state = viewModel.state.collectAsState().value
 
     LaunchedEffect(movieID) {
+        Log.d("TAG", "MovieDetailsScreen: $movieID")
         viewModel.loadMovieDetails(movieID)
     }
 
@@ -86,106 +99,223 @@ private fun MovieDetailsContent(
     interaction: MovieDetailsInteractionListener
 ) {
 
-    LazyColumn(
+    val scrollState = rememberScrollState()
+    Column(
         modifier = modifier
-            .background(Theme.color.background.screen)
-            .systemBarsPadding(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        contentPadding = PaddingValues(bottom = 30.dp)
-    ) {
-        item {
+        .systemBarsPadding()
+        .background(Theme.color.background.screen),
+    horizontalAlignment = Alignment.CenterHorizontally,
+   // contentPadding = PaddingValues(bottom = 30.dp)
+
+        ) {
+
+        Log.d("TAG", "MovieDetailsContent: ${state.movie.title}")
+
+        // Header
+        Column(Modifier.padding(horizontal = 16.dp).background(Theme.color.background.screen), horizontalAlignment = Alignment.CenterHorizontally,) {
             AppBar(
-                showBackButton = true, modifier = Modifier.padding(16.dp)
+                showBackButton = true,
+                onBackButtonClick = {}
+            )
+            MainMovieOrSeriesDetailsAnimatedContent(
+                type = TypeOfDetailsScreen.SERIES.name,
+                name = state.movie.title,
+                rating = state.movie.rating,
+                image = state.movie.posterUrl,
+                genres = state.movieGenres,
+                releaseYear = state.movie.releaseYear,
+                onClickAdd = interaction::onAddToCollectionClick,
+                onClickPlay = {},
+                isScrolled = scrollState.value > 0,
+                durationAnimation = 2000
             )
         }
 
-        item {
+        // Scrolling Body
+        Column(
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .verticalScroll(scrollState).background(Theme.color.background.screen),
+            verticalArrangement = Arrangement.spacedBy(24.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
             InfoSection(
                 modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
                 title = stringResource(R.string.storyline),
                 description = state.movie.description
             )
-        }
-        item {
-            state.cast.map { }
+
             StarCastSection(
                 title = stringResource(R.string.star_cast),
                 onShowMoreClick = {},
                 castList = state.cast
             )
-        }
-        item {
+
             StaffInfoSection(
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+                modifier = Modifier.padding(horizontal = 16.dp),
                 title = stringResource(R.string.behind_the_scenes),
                 staffList = state.crew
             )
-        }
-        item {
+
             MoviesListSection(
                 title = stringResource(R.string.you_might_also_like),
                 endText = stringResource(R.string.show_more),
                 movies = state.recommendedMovies,
-                onClickEndText = interaction::onShowMoreMoviesClick,
+                onClickEndText = {},
                 onClickPoster = {})
-        }
 
-        item {
             RatingSection(
-                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+                modifier = Modifier.padding(horizontal = 16.dp),
                 onClickCard = interaction::onGiveStarsClick
             )
-        }
 
-        item {
             AnimatedVisibility(
-                visible = state.movieReviews.take(3).isNotEmpty(),
+                visible = state.movieReviews.isNotEmpty(),
                 enter = fadeIn() + expandVertically(),
                 exit = fadeOut()
             ) {
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .padding(horizontal = 16.dp),
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Text(
-                        text = stringResource(R.string.top_reviews),
-                        color = Theme.color.shade.primary,
-                        style = Theme.textStyle.title.sm,
-                    )
+                InfoSection(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    title = stringResource(R.string.top_reviews),
+                    description = state.movie.description
+                )
+            }
 
-                    Text(
-                        text = stringResource(R.string.show_more),
-                        color = Theme.color.brand.primary,
-                        modifier = Modifier
-                            .padding(start = 12.dp)
-                            .clickable { interaction.onShowMoreReviewsClick() },
-                        style = Theme.textStyle.body.md.medium,
-                    )
+            AnimatedVisibility(state.movieReviews.isNotEmpty()) {
+
+                Column(
+                    modifier = Modifier.padding(horizontal = 16.dp),
+                    verticalArrangement = Arrangement.spacedBy(12.dp)
+                ) {
+                    for (index in 0..min(2, state.movieReviews.size - 1)) {
+                        val review = state.movieReviews[index]
+                        val padding = when (index) {
+                            0 -> Modifier.padding(vertical = 12.dp, horizontal = 16.dp)
+                            1 -> Modifier.padding(horizontal = 16.dp)
+                            2 -> Modifier.padding(top = 12.dp, start = 16.dp, end = 16.dp)
+                            else -> Modifier
+                        }
+                        ReviewCard(
+                            modifier = padding,
+                            rate = review.rating.toInt(),
+                            reviewText = review.content,
+                            reviewDate = review.createdAt,
+                            reviewerImageUrl = review.authorImageUrl,
+                            reviewerName = review.authorName,
+                            reviewerUsername = review.authorUserName
+                        )
+                    }
                 }
             }
-        }
 
-        itemsIndexed(state.movieReviews.take(3)) { index, review ->
-            val padding = when (index) {
-                0 -> Modifier.padding(vertical = 12.dp, horizontal = 16.dp)
-                1 -> Modifier.padding(horizontal = 16.dp)
-                2 -> Modifier.padding(top = 12.dp, start = 16.dp, end = 16.dp)
-                else -> Modifier
-            }
-            ReviewCard(
-                modifier = padding,
-                rate = review.rating,
-                reviewText = review.content,
-                reviewDate = review.createdAt,
-                reviewerImageUrl = review.authorImageUrl,
-                reviewerName = review.authorName,
-                reviewerUsername = review.authorUserName
+            Box(
+                Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
             )
         }
     }
+
+//    LazyColumn(
+//        modifier = modifier
+//            .background(Theme.color.background.screen)
+//            .systemBarsPadding(),
+//        horizontalAlignment = Alignment.CenterHorizontally,
+//        contentPadding = PaddingValues(bottom = 30.dp)
+//    ) {
+//        item {
+//            AppBar(
+//                showBackButton = true, modifier = Modifier.padding(16.dp)
+//            )
+//        }
+//
+//        item {
+//            InfoSection(
+//                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+//                title = stringResource(R.string.storyline),
+//                description = state.movie.description
+//            )
+//        }
+//        item {
+//            state.cast.map { }
+//            StarCastSection(
+//                title = stringResource(R.string.star_cast),
+//                onShowMoreClick = {},
+//                castList = state.cast
+//            )
+//        }
+//        item {
+//            StaffInfoSection(
+//                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+//                title = stringResource(R.string.behind_the_scenes),
+//                staffList = state.crew
+//            )
+//        }
+//        item {
+//            MoviesListSection(
+//                title = stringResource(R.string.you_might_also_like),
+//                endText = stringResource(R.string.show_more),
+//                movies = state.recommendedMovies,
+//                onClickEndText = interaction::onShowMoreMoviesClick,
+//                onClickPoster = {})
+//        }
+//
+//        item {
+//            RatingSection(
+//                modifier = Modifier.padding(horizontal = 16.dp, vertical = 24.dp),
+//                onClickCard = interaction::onGiveStarsClick
+//            )
+//        }
+//
+//        item {
+//            AnimatedVisibility(
+//                visible = state.movieReviews.take(3).isNotEmpty(),
+//                enter = fadeIn() + expandVertically(),
+//                exit = fadeOut()
+//            ) {
+//                Row(
+//                    modifier = Modifier
+//                        .fillMaxWidth()
+//                        .padding(horizontal = 16.dp),
+//                    horizontalArrangement = Arrangement.SpaceBetween,
+//                    verticalAlignment = Alignment.CenterVertically
+//                ) {
+//                    Text(
+//                        text = stringResource(R.string.top_reviews),
+//                        color = Theme.color.shade.primary,
+//                        style = Theme.textStyle.title.sm,
+//                    )
+//
+//                    Text(
+//                        text = stringResource(R.string.show_more),
+//                        color = Theme.color.brand.primary,
+//                        modifier = Modifier
+//                            .padding(start = 12.dp)
+//                            .clickable { interaction.onShowMoreReviewsClick() },
+//                        style = Theme.textStyle.body.md.medium,
+//                    )
+//                }
+//            }
+//        }
+//
+//        itemsIndexed(state.movieReviews.take(3)) { index, review ->
+//            val padding = when (index) {
+//                0 -> Modifier.padding(vertical = 12.dp, horizontal = 16.dp)
+//                1 -> Modifier.padding(horizontal = 16.dp)
+//                2 -> Modifier.padding(top = 12.dp, start = 16.dp, end = 16.dp)
+//                else -> Modifier
+//            }
+//            ReviewCard(
+//                modifier = padding,
+//                rate = review.rating,
+//                reviewText = review.content,
+//                reviewDate = review.createdAt,
+//                reviewerImageUrl = review.authorImageUrl,
+//                reviewerName = review.authorName,
+//                reviewerUsername = review.authorUserName
+//            )
+//        }
+//    }
 
 //    BaseBottomSheet(
 //        isVisible = state.isVisibleAddToCollectionBottomSheet,
