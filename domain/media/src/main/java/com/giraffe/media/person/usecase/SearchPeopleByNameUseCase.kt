f01f94ce7303c56f4eff912b6f8a55b8f496017f
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-07-19 03:56:00 +0300

    handle page in use case.

--- domain/media/src/main/java/com/giraffe/media/person/usecase/SearchPeopleByNameUseCase.kt
+++ domain/media/src/main/java/com/giraffe/media/person/usecase/SearchPeopleByNameUseCase.kt
@@ -5,5 +5,6 @@ import com.giraffe.media.person.repository.PersonRepository
 class SearchPeopleByNameUseCase(
     private val repository: PersonRepository
 ) {
-    suspend operator fun invoke(personName: String) = repository.searchByName(personName)
+    suspend operator fun invoke(personName: String, page: Int = 1) =
+        repository.searchByName(personName, page)
 }
