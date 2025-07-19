3b608b64d289933e132f6759aaa4d3c7d36ed601
bilalazzam <blalazam065@gmail.com>
2025-07-19 15:52:42 +0300

    implement explore api

--- api/explore/src/main/java/com/giraffe/explore/ExploreApi.kt
+++ api/explore/src/main/java/com/giraffe/explore/ExploreApi.kt
@@ -1,5 +1,23 @@
 package com.giraffe.explore
 
 
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
 interface ExploreApi {
+
+    fun navigateToSearch()
+
+    fun navigateToSearchResult(query: String)
+
+    fun navigateToDiscover()
+
+
+    @Composable
+    fun ExploreContainer(
+        modifier: Modifier = Modifier,
+        navigateToMovieDetails: (Int) -> Unit = {},
+        navigateToSeriesDetails: (Int) -> Unit = {},
+        navigateToCastDetails: (Int) -> Unit = {}
+    )
 }
