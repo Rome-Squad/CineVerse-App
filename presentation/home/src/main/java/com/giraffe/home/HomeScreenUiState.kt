15c6f08e52a24fc093622e01eb06723bf8a35668
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-07-20 20:23:04 +0300

    create home ui state and ui model

---
+++ presentation/home/src/main/java/com/giraffe/home/HomeScreenUiState.kt
@@ -0,0 +1,27 @@
+package com.giraffe.home
+
+
+data class HomeScreenUiState(
+
+    val userName: String = "",
+
+    val matchVibes: List<HomeUiModel> = emptyList(),
+
+    val popularity: List<PopularMediaUiModel> = emptyList(),
+
+    val recentlyReleased: List<HomeUiModel> = emptyList(),
+
+    val topRated: List<HomeUiModel> = emptyList(),
+
+    val upcomingMovies: List<HomeUiModel> = emptyList(),
+
+    val recentlyViewed: List<HomeUiModel> = emptyList(),
+
+    val collections: List<YourCollectionUiModel> = emptyList(),
+
+    val featuredCollections: List<FeaturedCollectionUiModel> = emptyList(),
+
+    val isLoading: Boolean = false,
+
+    val isError: Boolean = false
+)
---
+++ presentation/home/src/main/java/com/giraffe/home/HomeUiModel.kt
@@ -0,0 +1,34 @@
+package com.giraffe.home
+
+enum class MediaType {
+    MOVIE,
+    SERIES
+}
+
+data class HomeUiModel(
+    val id: Int,
+    val title: String,
+    val posterUrl: String,
+    val rating: Float,
+    val mediaType: MediaType
+)
+
+data class YourCollectionUiModel(
+    val id: Int,
+    val title: String,
+    val numberOfItems: Int
+)
+
+data class FeaturedCollectionUiModel(
+    val id: Int,
+    val title: String,
+    val backgroundImageUrl: String
+)
+data class PopularMediaUiModel(
+    val id: Int,
+    val title: String,
+    val posterUrl: String,
+    val genres: List<String>,
+    val rating: Float ,
+    val mediaType: MediaType
+)
