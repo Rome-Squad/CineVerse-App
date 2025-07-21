cd9ac8cf150f8f7499485f329d58e89488524c8b
Mohamed Ashraf <ma1261327@gmail.com>
2025-07-21 17:25:11 +0300

    create recommended screen without impl for Transition between LazyColumn to LazyVerticalGrid Series

---
+++ presentation/details/src/main/java/com/giraffe/details/screens/recommended/series/RecommendedSeriesContent.kt
@@ -0,0 +1,60 @@
+package com.giraffe.details.screens.recommended.series
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import androidx.paging.compose.LazyPagingItems
+import com.giraffe.designsystem.composable.AppBar
+import com.giraffe.designsystem.composable.ViewToggle
+import com.giraffe.designsystem.theme.Theme
+import com.giraffe.details.R
+import com.giraffe.details.models.SeriesUi
+
+@Composable
+fun RecommendedSeriesContent(
+    title: String,
+    lazyPagingItems: LazyPagingItems<SeriesUi>,
+    onBackButtonClick: () -> Unit,
+    interaction: RecommendedInteractionListener,
+    modifier: Modifier = Modifier,
+) {
+    var isGridSelected by rememberSaveable { mutableStateOf(true) }
+
+    Box {
+        Column(
+            modifier = modifier
+                .background(Theme.color.background.screen)
+                .statusBarsPadding(),
+        ) {
+            AppBar(
+                title = title,
+                caption = stringResource(R.string.because_you_watched),
+                showBackButton = true,
+                modifier = Modifier.padding(8.dp),
+                onBackButtonClick = onBackButtonClick,
+            )
+
+            // Here section not implement yet
+            // Transition between LazyColumn to LazyVerticalGrid Series
+        }
+
+        ViewToggle(
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp),
+            isListSelected = !isGridSelected,
+            onGridSelected = { isGridSelected = !isGridSelected }
+        )
+    }
+}
