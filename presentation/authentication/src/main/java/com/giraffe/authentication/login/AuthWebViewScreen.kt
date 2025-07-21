3c5b272e0c63182023336f811444ccdb96f4c109
Doaa Abdelnaser <doaaabdelnaser151998@gmail.com>
2025-07-21 21:48:44 +0300

    refactor AuthWebViewScreen to use Accompanist WebView for improved navigation handling

--- presentation/authentication/src/main/java/com/giraffe/authentication/login/AuthWebViewScreen.kt
+++ presentation/authentication/src/main/java/com/giraffe/authentication/login/AuthWebViewScreen.kt
@@ -2,45 +2,39 @@ package com.giraffe.authentication.login
 
 import android.webkit.WebResourceRequest
 import android.webkit.WebView
-import android.webkit.WebViewClient
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.viewinterop.AndroidView
+
 import androidx.navigation.NavController
+import com.google.accompanist.web.WebView
+import com.google.accompanist.web.AccompanistWebViewClient
+
+import com.google.accompanist.web.rememberWebViewState
 
 @Composable
 fun AuthWebViewScreen(navController: NavController) {
     val signUpUrl = "https://www.themoviedb.org/signup"
-    var currentUrl = remember { signUpUrl }
-
-    AndroidView(
-        factory = { context ->
-            WebView(context).apply {
-                loadUrl(currentUrl)
-                webViewClient = object : WebViewClient() {
-                    override fun onPageFinished(view: WebView?, url: String?) {
-                        super.onPageFinished(view, url)
-                        url?.let {
-                            currentUrl = it
-                            if (it.contains("themoviedb.org/login")) {
-                                // Go back to previous screen if login page is reached
-                                navController.popBackStack()
-                            }
-                        }
-                    }
-
-                    override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
-                        //  URL should be handled by WebView or if we need to navigate manually
-                        request?.url?.let {
-                            if (it.toString().contains("themoviedb.org/login")) {
-                                navController.popBackStack()
-                                return true
-                            }
-                        }
-                        return super.shouldOverrideUrlLoading(view, request)
-                    }
+    var webViewState = rememberWebViewState(url=signUpUrl)
+    WebView(
+        state = webViewState,
+        onCreated = { it.settings.javaScriptEnabled = true },
+        client = object :  AccompanistWebViewClient(){
+            override fun onPageFinished(view: WebView, url: String?) {
+                super.onPageFinished(view, url)
+                if (url?.contains("themoviedb.org/login") == true) {
+                    navController.popBackStack()
                 }
             }
-        },
+
+            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+                //  URL should be handled by WebView or if we need to navigate manually
+                request?.url?.let {
+                    if (it.toString().contains("themoviedb.org/login")) {
+                        navController.popBackStack()
+                        return true
+                    }
+                }
+                return super.shouldOverrideUrlLoading(view, request)
+            }
+        }
     )
 }
