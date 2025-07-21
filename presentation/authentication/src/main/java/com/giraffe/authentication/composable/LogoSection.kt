686cf07849d796b8bdc6cd93bb2de1d3ea94cde8
HendSayed25 <hendsayed853@gmail.com>
2025-07-21 12:57:52 +0300

    Fix: Extract strings in LogoSection

--- presentation/authentication/src/main/java/com/giraffe/authentication/composable/LogoSection.kt
+++ presentation/authentication/src/main/java/com/giraffe/authentication/composable/LogoSection.kt
@@ -1,7 +1,6 @@
 package com.giraffe.authentication.composable
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -9,6 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giraffe.designsystem.R
@@ -25,14 +25,14 @@ fun LogoSection(
 
         Image(
             painter = painterResource(R.drawable.colored_cineverse_logo),
-            contentDescription = "logo",
+            contentDescription = stringResource(com.giraffe.authentication.R.string.logo),
             modifier = Modifier
                 .padding(top = 48.dp, bottom = 8.dp)
                 .size(104.dp)
         )
 
         Text(
-            text = "Welcome back to CineVerse!",
+            text = stringResource(com.giraffe.authentication.R.string.welcome_back_to_cineverse),
             style = Theme.textStyle.title.lg,
             modifier = Modifier.padding(bottom = 48.dp)
         )
