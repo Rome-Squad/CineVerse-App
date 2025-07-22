ab49d4a374ef920eee45470cd285e9fdf162e629
Mohamed Ashraf <ma1261327@gmail.com>
2025-07-22 12:16:09 +0300

    fix navigation between recommended series and series details

--- presentation/details/src/main/java/com/giraffe/details/screens/recommended/series/RecommendedSeriesContent.kt
+++ presentation/details/src/main/java/com/giraffe/details/screens/recommended/series/RecommendedSeriesContent.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.runtime.Composable
@@ -57,6 +58,7 @@ fun RecommendedSeriesContent(
         ViewToggle(
             modifier = Modifier
                 .align(Alignment.BottomEnd)
+                .navigationBarsPadding()
                 .padding(16.dp),
             isListSelected = !isGridSelected,
             onGridSelected = { isGridSelected = !isGridSelected }
