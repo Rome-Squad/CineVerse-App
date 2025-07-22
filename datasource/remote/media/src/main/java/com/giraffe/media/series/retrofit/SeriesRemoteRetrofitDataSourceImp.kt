6f12f3121734d23e4ca263735806caa7ce5a229a
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-07-22 15:06:13 +0300

    feat: Implement Retrofit-based remote data source for series

---
+++ datasource/remote/media/src/main/java/com/giraffe/media/series/retrofit/SeriesRemoteRetrofitDataSourceImp.kt
@@ -0,0 +1,30 @@
+package com.giraffe.media.series.retrofit
+
+import com.giraffe.media.series.datasource.remote.SeriesRemoteDataSource
+import com.giraffe.media.series.datasource.remote.dto.SeriesDetailsDto
+import com.giraffe.media.util.RetrofitRequestBuilder
+
+class SeriesRemoteRetrofitDataSourceImp(
+    private val builder: RetrofitRequestBuilder<SeriesApiServiceRetrofit>
+) : SeriesRemoteDataSource {
+
+    override suspend fun getSeriesByName(name: String, page: Int) =
+        builder.get { getSeriesByName(name, page) }.results
+
+    override suspend fun getSeriesByGenre(genreId: Int, page: Int) =
+        builder.get {
+            getSeriesByGenre(if (genreId == -1) "" else genreId.toString())
+        }.results
+
+    override suspend fun getGenres() =
+        builder.get { getGenres() }.genres
+
+    override suspend fun getSeriesDetails(seriesId: Int): SeriesDetailsDto =
+        builder.get { getSeriesDetails(seriesId) }
+
+    override suspend fun getSeriesReviews(seriesId: Int, page: Int) =
+        builder.get { getSeriesReviews(seriesId, page) }.results
+
+    override suspend fun getSeriesRecommendations(seriesId: Long, page: Int) =
+        builder.get { getSeriesRecommendations(seriesId, page) }.results
+}
