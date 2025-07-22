12549c2e6c869ca7335ec0c74c503b9cf0e760ed
Mohamed Ashraf <ma1261327@gmail.com>
2025-07-22 11:44:05 +0300

    add transition animation

--- presentation/details/src/main/java/com/giraffe/details/screens/recommended/series/TransitionBetweenLazyColumnToLazyVerticalGridSeries.kt
+++ presentation/details/src/main/java/com/giraffe/details/screens/recommended/series/TransitionBetweenLazyColumnToLazyVerticalGridSeries.kt
@@ -1,4 +1,4 @@
-package com.giraffe.details.components.recommended
+package com.giraffe.details.screens.recommended.series
 
 import androidx.compose.animation.AnimatedContent
 import androidx.compose.animation.ExperimentalSharedTransitionApi
@@ -12,16 +12,12 @@ import androidx.compose.animation.scaleIn
 import androidx.compose.animation.togetherWith
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.snapshotFlow
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.paging.compose.LazyPagingItems
 import com.giraffe.details.components.recomended.PosterHorizontal
@@ -31,50 +27,32 @@ import com.giraffe.details.models.toPoster
 
 @OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
-fun TransitionLazyColumnToGrid(
+fun TransitionBetweenLazyColumnToLazyVerticalGridSeries(
     lazyPagingItems: LazyPagingItems<SeriesUi>,
     isListSelected: Boolean = false,
     contentPadding: PaddingValues = PaddingValues(vertical = 16.dp),
-    onScroll: (isScrollingUp: Boolean) -> Unit = {}
+    onScroll: (isScrollingUp: Boolean) -> Unit = {},
+    onItemClick: (Int) -> Unit,
 ) {
     val listState = rememberLazyListState()
     val gridState = rememberLazyGridState()
+    ObserveScrollDirection(
+        listState,
+        { it.firstVisibleItemIndex },
+        { it.firstVisibleItemScrollOffset },
+        onScroll
+    )
+    ObserveScrollDirection(
+        gridState,
+        { it.firstVisibleItemIndex },
+        { it.firstVisibleItemScrollOffset },
+        onScroll
+    )
 
-    LaunchedEffect(listState) {
-        var previousIndex = listState.firstVisibleItemIndex
-        var previousScrollOffset = listState.firstVisibleItemScrollOffset
-        snapshotFlow { listState.firstVisibleItemIndex to listState.firstVisibleItemScrollOffset }
-            .collect { (index, offset) ->
-                if (index > previousIndex || (index == previousIndex && offset > previousScrollOffset)) {
-                    onScroll(false)
-                } else if (index < previousIndex || offset < previousScrollOffset) {
-                    onScroll(true)
-                }
-                previousIndex = index
-                previousScrollOffset = offset
-            }
-    }
-
-    LaunchedEffect(gridState) {
-        var previousIndex = gridState.firstVisibleItemIndex
-        var previousScrollOffset = gridState.firstVisibleItemScrollOffset
-        snapshotFlow { gridState.firstVisibleItemIndex to gridState.firstVisibleItemScrollOffset }
-            .collect { (index, offset) ->
-                if (index > previousIndex || (index == previousIndex && offset > previousScrollOffset)) {
-                    onScroll(false)
-                } else if (index < previousIndex || offset < previousScrollOffset) {
-                    onScroll(true)
-                }
-                previousIndex = index
-                previousScrollOffset = offset
-            }
-    }
 
     SharedTransitionLayout {
         AnimatedContent(
-            modifier = Modifier.padding(horizontal = 16.dp),
             targetState = isListSelected,
-            label = "ViewToggleAnimation",
             transitionSpec = {
                 (fadeIn(animationSpec = tween(220, delayMillis = 90, easing = EaseIn)) +
                         scaleIn(
@@ -91,11 +69,12 @@ fun TransitionLazyColumnToGrid(
                     contentPadding = contentPadding
                 ) {
                     items(lazyPagingItems.itemCount) { index ->
-                        lazyPagingItems[index]?.let { poster ->
+                        lazyPagingItems[index]?.let { series ->
                             PosterHorizontal(
-                                poster = poster.toPoster(),
+                                poster = series.toPoster(),
                                 animatedVisibilityScope = this@AnimatedContent,
                                 sharedTransitionScope = this@SharedTransitionLayout,
+                                onClick = { onItemClick(series.id) }
                             )
                         }
                     }
@@ -103,17 +82,18 @@ fun TransitionLazyColumnToGrid(
             } else {
                 LazyVerticalGrid(
                     state = gridState,
-                    columns = GridCells.Fixed(2),
+                    columns = GridCells.Adaptive(165.dp),
                     verticalArrangement = Arrangement.spacedBy(16.dp),
                     horizontalArrangement = Arrangement.spacedBy(16.dp),
                     contentPadding = contentPadding
                 ) {
                     items(lazyPagingItems.itemCount) { index ->
-                        lazyPagingItems[index]?.let { poster ->
+                        lazyPagingItems[index]?.let { series ->
                             PosterVertically(
-                                poster = poster.toPoster(),
+                                poster = series.toPoster(),
                                 animatedVisibilityScope = this@AnimatedContent,
                                 sharedTransitionScope = this@SharedTransitionLayout,
+                                onClick = { onItemClick(series.id) }
                             )
                         }
                     }
