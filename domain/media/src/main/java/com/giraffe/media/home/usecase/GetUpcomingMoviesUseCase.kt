0a566df700f49ae77072557dcee0883d68884987
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-07-23 11:18:49 +0300

    move movie use cases from home to movie package.

--- domain/media/src/main/java/com/giraffe/media/home/usecase/GetUpcomingMoviesUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.giraffe.media.home.usecase
-
-import com.giraffe.media.home.repository.HomeRepository
-import com.giraffe.media.movies.entity.Movie
-
-class GetUpcomingMoviesUseCase(private val homeRepository: HomeRepository) {
-    operator fun invoke(): List<Movie> = homeRepository.getUpcomingMovies()
-}
