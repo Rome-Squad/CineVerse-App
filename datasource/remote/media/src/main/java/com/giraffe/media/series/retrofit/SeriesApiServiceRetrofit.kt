1db2154a47d3e3834950bc34d3953ca9f4d31a26
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-07-23 20:13:57 +0300

    add and get rating

--- datasource/remote/media/src/main/java/com/giraffe/media/series/retrofit/SeriesApiServiceRetrofit.kt
+++ datasource/remote/media/src/main/java/com/giraffe/media/series/retrofit/SeriesApiServiceRetrofit.kt
@@ -1,11 +1,15 @@
 package com.giraffe.media.series.retrofit
 
+import com.giraffe.media.movie.datasource.remote.dto.RatedMoviesResponse
+import com.giraffe.media.movie.datasource.remote.dto.RatingRequest
 import com.giraffe.media.series.datasource.remote.dto.SeriesDetailsDto
 import com.giraffe.media.series.response.GenresResponse
 import com.giraffe.media.series.response.SeriesResponse
 import com.giraffe.media.series.response.SeriesReviewsResponse
 import retrofit2.Response
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.Path
 import retrofit2.http.Query
 
@@ -42,6 +46,20 @@ interface SeriesApiServiceRetrofit {
         @Query(PAGE) page: Int = 1
     ): Response<SeriesResponse>
 
+    @POST("$TV/{$SERIES_ID}/$RATING")
+    suspend fun rateSeries(
+        @Path(SERIES_ID) seriesId: Int,
+        @Query(SESSION_ID) sessionId: String,
+        @Body request: RatingRequest
+    ): Response<Unit>
+
+
+    @GET("$TV/{$SERIES_ID}/$ACCOUNT_STATES")
+    suspend fun getSeriesRating(
+        @Path(SERIES_ID) seriesId: Int,
+        @Query(SESSION_ID) sessionId: String
+    ): Response<RatedMoviesResponse>
+
     companion object {
         const val SEARCH_TV = "search/tv"
         const val DISCOVER_TV = "discover/tv"
@@ -55,5 +73,9 @@ interface SeriesApiServiceRetrofit {
         const val SERIES_ID = "seriesId"
         const val REVIEWS = "reviews"
         const val RECOMMENDATIONS = "recommendations"
+
+        const val RATING = "rating"
+        const val ACCOUNT_STATES = "account_states"
+        const val SESSION_ID = "session_id"
     }
 }
