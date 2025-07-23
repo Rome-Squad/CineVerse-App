272f0cc41574a387e53933ec4550c9384ae31909
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-07-23 11:19:59 +0300

    add get popularity movies use case.

---
+++ domain/media/src/main/java/com/giraffe/media/movies/usecase/GetPopularityMoviesUseCase.kt
@@ -0,0 +1,8 @@
+package com.giraffe.media.movies.usecase
+
+import com.giraffe.media.movies.entity.Movie
+import com.giraffe.media.movies.repository.MoviesRepository
+
+class GetPopularityMoviesUseCase(private val repository: MoviesRepository) {
+    suspend operator fun invoke(): List<Movie> = repository.getPopularityMovies()
+}
