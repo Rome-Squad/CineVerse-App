188963d0cdafe62df38d029049dc32d4cc456c0e
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-07-25 02:28:48 +0300

    use request builder in api service and remote data

--- datasource/remote/user/src/main/java/com/giraffe/user/retrofit/UserRemoteDataSourceImplRetrofit.kt
+++ datasource/remote/user/src/main/java/com/giraffe/user/retrofit/UserRemoteDataSourceImplRetrofit.kt
@@ -3,21 +3,23 @@ package com.giraffe.user.retrofit
 import com.giraffe.repository.datasource.UserRemoteDataSource
 import com.giraffe.user.dto.CreateSessionRequest
 import com.giraffe.user.dto.ValidateTokenRequest
+import com.giraffe.user.util.RetrofitUserRequestBuilder
 
 class UserRemoteDataSourceImplRetrofit(
-    private val UserApiServiceRetrofit: UserApiServiceRetrofit
+    private val retrofitRequestBuilder: RetrofitUserRequestBuilder<UserApiServiceRetrofit>
 ) : UserRemoteDataSource {
 
-    override suspend fun createRequestToken(): String = UserApiServiceRetrofit.createRequestToken().requestToken
+    override suspend fun createRequestToken(): String =
+        retrofitRequestBuilder.get { createRequestToken() }.requestToken
 
     override suspend fun validateTokenWithLogin(token: String, user: String, pass: String): String {
         val request = ValidateTokenRequest(username = user, password = pass, requestToken = token)
-        return UserApiServiceRetrofit.validateTokenWithLogin(request).requestToken
+        return retrofitRequestBuilder.post { validateTokenWithLogin(request) }.requestToken
     }
 
     override suspend fun createSession(token: String): String {
         val request = CreateSessionRequest(requestToken = token)
-        return UserApiServiceRetrofit.createSession(request).sessionId
+        return retrofitRequestBuilder.post { createSession(request) }.sessionId
     }
 
 }
