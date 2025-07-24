c8daa59ef30e361f592a1787e4efda859915cf99
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-07-25 02:25:24 +0300

    create request builder

--- datasource/remote/user/src/main/java/com/giraffe/user/util/AuthInterceptor.kt
+++ datasource/remote/user/src/main/java/com/giraffe/user/util/AuthInterceptor.kt
@@ -1,4 +1,4 @@
-package com.giraffe.user
+package com.giraffe.user.util
 
 import okhttp3.Interceptor
 import okhttp3.Response
---
+++ datasource/remote/user/src/main/java/com/giraffe/user/util/RetrofitUserRequestBuilder.kt
@@ -0,0 +1,61 @@
+package com.giraffe.user.util
+
+import com.giraffe.repository.exceptions.ApiDataException
+import com.giraffe.repository.exceptions.AuthDataException
+import com.giraffe.repository.exceptions.InvalidIdDataException
+import com.giraffe.repository.exceptions.NoInternetDataException
+import com.giraffe.repository.exceptions.RequestTimeoutDataException
+import com.giraffe.repository.exceptions.SerializationDataException
+import com.giraffe.repository.exceptions.UnknownNetworkDataException
+import retrofit2.Response
+import java.io.IOException
+
+class RetrofitUserRequestBuilder<API>(
+    val api: API
+) {
+
+    suspend inline fun <reified T> get(
+        crossinline call: suspend API.() -> Response<T>
+    ): T {
+        return safeCall { api.call() }
+    }
+
+    suspend inline fun <reified T> post(
+        crossinline call: suspend API.() -> Response<T>
+    ): T {
+        return safeCall { api.call() }
+    }
+
+    companion object {
+        suspend inline fun <reified T> safeCall(
+            crossinline execute: suspend () -> Response<T>
+        ): T {
+            return try {
+                val response = execute()
+
+                if (response.isSuccessful) {
+                    val body = response.body()
+                    body ?: throw SerializationDataException()
+                } else {
+                    val errorBody = try {
+                        response.errorBody()?.string() ?: "Unknown error"
+                    } catch (e: Exception) {
+                        "Unknown error"
+                    }
+
+                    throw ApiDataException(response.code())
+                }
+            } catch (e: Throwable) {
+                throw mapToMediaException(e)
+            }
+        }
+
+        fun mapToMediaException(e: Throwable): AuthDataException = when (e) {
+            is AuthDataException -> e
+            is java.net.SocketTimeoutException -> RequestTimeoutDataException()
+            is IOException -> NoInternetDataException()
+            is IllegalArgumentException -> InvalidIdDataException()
+            else -> UnknownNetworkDataException()
+        }
+    }
+}
