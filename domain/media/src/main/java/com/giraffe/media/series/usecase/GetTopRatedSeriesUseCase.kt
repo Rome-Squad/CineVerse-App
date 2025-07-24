d34b553ff81ffd2d29258d29b543e8a9d7bc29cb
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-07-24 18:07:14 +0300

    update get top rated series use case to add page.

--- domain/media/src/main/java/com/giraffe/media/series/usecase/GetTopRatedSeriesUseCase.kt
+++ domain/media/src/main/java/com/giraffe/media/series/usecase/GetTopRatedSeriesUseCase.kt
@@ -4,5 +4,5 @@ import com.giraffe.media.series.entity.Series
 import com.giraffe.media.series.repository.SeriesRepository
 
 class GetTopRatedSeriesUseCase(private val repository: SeriesRepository) {
-    suspend operator fun invoke(): List<Series> = repository.getTopRatedSeries()
+    suspend operator fun invoke(page:Int): List<Series> = repository.getTopRatedSeries(page)
 }
