70b4796a76c8df4a4f29a7fed359386e0b00bccf
Doaa Abdelnaser <doaaabdelnaser151998@gmail.com>
2025-07-25 16:49:35 +0300

    enhance login screen with scroll support and error handling

--- presentation/authentication/src/main/java/com/giraffe/authentication/screen/LoginScreen.kt
+++ presentation/authentication/src/main/java/com/giraffe/authentication/screen/LoginScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -75,6 +77,7 @@ private fun LoginContent(
     Column(
         modifier = modifier
             .fillMaxSize()
+            .verticalScroll(rememberScrollState())
             .background(Theme.color.background.screen)
             .systemBarsPadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
--- presentation/authentication/src/main/java/com/giraffe/authentication/screen/LoginViewModel.kt
+++ presentation/authentication/src/main/java/com/giraffe/authentication/screen/LoginViewModel.kt
@@ -16,16 +16,22 @@ class LoginViewModel(
 
     override fun onUsernameChanged(username: String) {
         updateState {
-            it.copy(username = username)
+            it.copy(
+                username = username,
+                usernameErrorMessage = null
+            )
         }
     }
+
 
     override fun onPasswordChanged(password: String) {
         updateState {
-            it.copy(password = password)
+            it.copy(
+                password = password,
+                passwordErrorMessage = null
+            )
         }
     }
-
     override fun onLoginClick() {
         updateState { it.copy(isLoadingLogin = true) }
 
@@ -41,25 +47,31 @@ class LoginViewModel(
     }
 
     private fun onLoginError(throwable: Throwable) {
-        if (throwable is EmptyUsernameException) updateState {
-            it.copy(
-                usernameErrorMessage = mapExceptionToStringRes(
-                    throwable
+        if (throwable is EmptyUsernameException) {
+            updateState {
+                it.copy(
+                    usernameErrorMessage = mapExceptionToStringRes(throwable),
+                    passwordErrorMessage = null
                 )
-            )
+            }
         }
 
-        if (throwable is InvalidPasswordException) updateState {
-            it.copy(
-                passwordErrorMessage = mapExceptionToStringRes(throwable)
-            )
+        if (throwable is InvalidPasswordException) {
+            updateState {
+                it.copy(
+                    passwordErrorMessage = mapExceptionToStringRes(throwable),
+                    usernameErrorMessage = null
+                )
+            }
         }
 
-        if(throwable is InvalidUsernameOrPasswordException) updateState {
-            it.copy(
-                usernameErrorMessage = mapExceptionToStringRes(throwable),
-                passwordErrorMessage = mapExceptionToStringRes(throwable)
-            )
+        if (throwable is InvalidUsernameOrPasswordException) {
+            updateState {
+                it.copy(
+                    usernameErrorMessage = mapExceptionToStringRes(throwable),
+                    passwordErrorMessage = mapExceptionToStringRes(throwable)
+                )
+            }
         }
 
         updateState { it.copy(isLoadingLogin = false) }
