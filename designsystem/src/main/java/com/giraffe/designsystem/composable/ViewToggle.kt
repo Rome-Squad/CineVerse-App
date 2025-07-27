2b3e7e0be21c024836c4ac5273326b24a6c86262
Doaa Abdelnaser <doaaabdelnaser151998@gmail.com>
2025-07-27 11:47:02 +0300

    remove hover effect from view toggle component

--- designsystem/src/main/java/com/giraffe/designsystem/composable/ViewToggle.kt
+++ designsystem/src/main/java/com/giraffe/designsystem/composable/ViewToggle.kt
@@ -6,6 +6,7 @@ import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.offset
@@ -108,7 +109,11 @@ private fun ToggleButtonIcon(
     Crossfade(
         modifier = modifier
             .clip(RoundedCornerShape(Theme.radius.s))
-            .clickable(onClick = onClick)
+            .clickable(
+                onClick = onClick,
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() }
+            )
             .size(40.dp)
             .wrapContentSize()
             .padding(horizontal = 10.dp),
