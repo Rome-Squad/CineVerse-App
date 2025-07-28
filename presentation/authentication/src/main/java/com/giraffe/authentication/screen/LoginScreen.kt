83bf82af44ba0378c591372061b3098d78b6bd53
Mohannad Ahmed <mohannad199344@gmail.com>
2025-07-28 19:23:04 +0300

    Fix: Clears focus when login button is clicked in LoginForm

--- presentation/authentication/src/main/java/com/giraffe/authentication/screen/LoginScreen.kt
+++ presentation/authentication/src/main/java/com/giraffe/authentication/screen/LoginScreen.kt
@@ -1,11 +1,9 @@
 package com.giraffe.authentication.screen
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
@@ -17,7 +15,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.giraffe.authentication.R
 import com.giraffe.authentication.composable.LoginForm
