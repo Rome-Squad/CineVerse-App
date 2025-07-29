d4bf5628b988ac620c81bbe62601cfcf5894f375
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-07-29 21:10:29 +0300

    feat: Integrate Hilt into MovieDetailsViewModel for dependency injection

--- presentation/details/src/main/java/com/giraffe/details/screens/moviedetails/MovieDetailsViewModel.kt
+++ presentation/details/src/main/java/com/giraffe/details/screens/moviedetails/MovieDetailsViewModel.kt
@@ -20,9 +20,11 @@ import com.giraffe.media.movies.usecase.SetMovieRecentUseCase
 import com.giraffe.media.person.entity.Person
 import com.giraffe.media.person.entity.PersonType
 import com.giraffe.media.person.usecase.GetPeopleByMovieIdUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
 
-
-class MovieDetailsViewModel(
+@HiltViewModel
+class MovieDetailsViewModel @Inject constructor(
     val getMovieDetails: GetMovieDetailsUseCase,
     val getMovieGenres: GetMovieGenresUseCase,
     val getMovieReviewsUseCase: GetMovieReviewsUseCase,
