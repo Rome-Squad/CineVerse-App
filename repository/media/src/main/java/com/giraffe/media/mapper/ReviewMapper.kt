22154906d496eeb00cdcf4de30a17a7e533031d5
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-07-29 22:36:44 +0300

    convert string to time make it reusable

--- repository/media/src/main/java/com/giraffe/media/mapper/ReviewMapper.kt
+++ repository/media/src/main/java/com/giraffe/media/mapper/ReviewMapper.kt
@@ -3,23 +3,16 @@ package com.giraffe.media.mapper
 import com.giraffe.media.dto.ReviewDto
 import com.giraffe.media.entity.Review
 import com.giraffe.media.utils.BASE_IMAGE_URL
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
+import com.giraffe.media.utils.toLocalDateTime
 import kotlin.time.ExperimentalTime
-import kotlin.time.Instant
 
 @OptIn(ExperimentalTime::class)
 fun ReviewDto.toEntity() = Review(
-    id = this.id,
-    authorImageUrl = BASE_IMAGE_URL + this.authorDetails.avatarPath.toString(),
-    authorName = this.authorDetails.name?.takeIf { it.isNotBlank() } ?: this.author,
-    authorUserName = this.authorDetails.username,
-    content = this.content,
-    rating = (this.authorDetails.rating ?: 0f).toInt(),
-    createdAt = try {
-        val instant = Instant.parse(this.createdAt)
-        instant.toLocalDateTime(TimeZone.UTC)
-    } catch (e: Exception) {
-        null
-    },
+    id = id,
+    authorImageUrl = BASE_IMAGE_URL + authorDetails.avatarPath.toString(),
+    authorName = authorDetails.name?.takeIf { it.isNotBlank() } ?: author,
+    authorUserName = authorDetails.username,
+    content = content,
+    rating = (authorDetails.rating ?: 0f).toInt(),
+    createdAt = createdAt.toLocalDateTime(),
 )
