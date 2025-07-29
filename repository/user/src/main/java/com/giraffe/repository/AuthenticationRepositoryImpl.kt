397cdb55bdd04d974d2df6bb132e4f0c43d1ae0c
bilalazzam <blalazam065@gmail.com>
2025-07-29 16:27:42 +0300

    update authentication repository

---
+++ repository/user/src/main/java/com/giraffe/repository/AuthenticationRepositoryImpl.kt
@@ -0,0 +1,24 @@
+package com.giraffe.repository
+
+import android.util.Log
+import com.giraffe.repository.datasource.local.AuthenticationRemoteDataSource
+import com.giraffe.repository.datasource.remote.AuthenticationLocalDataSource
+import com.giraffe.repository.utils.SafeCall
+import com.giraffe.user.repository.AuthRepository
+
+class AuthenticationRepositoryImpl(
+    private val remoteDataSource: AuthenticationRemoteDataSource,
+    private val localDataSource: AuthenticationLocalDataSource
+) : AuthRepository {
+    override suspend fun login(username: String, password: String) = SafeCall {
+        val requestToken = remoteDataSource.createRequestToken()
+
+        val validatedToken =
+            remoteDataSource.validateTokenWithLogin(requestToken, username, password)
+
+        val sessionId = remoteDataSource.createSession(validatedToken)
+        Log.d("TAG", "session id: $sessionId")
+
+        localDataSource.saveSessionId(sessionId)
+    }
+}
