523f4228b4b67b5dbc767cc0b5b062a335ba915b
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-07-30 08:08:29 +0300

    create response package and move all reviews to it

--- datasource/remote/media/src/main/java/com/giraffe/media/response/AllReviewsResponse.kt
+++ datasource/remote/media/src/main/java/com/giraffe/media/response/AllReviewsResponse.kt
@@ -1,11 +1,11 @@
-package com.giraffe.media.util
+package com.giraffe.media.response
 
 import com.giraffe.media.dto.ReviewDto
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AllReviewsDto(
+data class AllReviewsResponse(
     val id: Int,
     val page: Int,
     val results: List<ReviewDto>,
