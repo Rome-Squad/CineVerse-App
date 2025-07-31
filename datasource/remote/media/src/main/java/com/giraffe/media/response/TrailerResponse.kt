04ed9f9260456bba5e3d20cd58d84a88961d7ccc
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-08-01 00:41:52 +0300

    Remove iso6391 and iso31661 fields from TrailerResponse
    The fields `iso6391` and `iso31661` were removed from the `TrailerResponse` data class as they were not being used.

--- datasource/remote/media/src/main/java/com/giraffe/media/response/TrailerResponse.kt
+++ datasource/remote/media/src/main/java/com/giraffe/media/response/TrailerResponse.kt
@@ -12,10 +12,6 @@ data class TrailerResponse(
     var size: Int? = null,
     var type: String? = null,
     var official: Boolean? = null,
-    @SerialName("iso_639_1")
-    var iso6391: String? = null,
-    @SerialName("iso_3166_1")
-    var iso31661: String? = null,
     @SerialName("published_at")
     var publishedAt: String? = null,
 )
