8688276621858f339fe7726459e70960bebc91b8
Hend Sayed <hendsayed853@gmail.com>
2025-07-31 19:34:34 +0300

    Refactor: Remove key from LazyRow and LazyVerticalGrid items

--- presentation/details/src/main/java/com/giraffe/details/components/TransitionBetweenColumnAndVerticalGrid.kt
+++ presentation/details/src/main/java/com/giraffe/details/components/TransitionBetweenColumnAndVerticalGrid.kt
@@ -52,7 +52,7 @@ fun TransitionBetweenColumnAndVerticalGrid(
                     verticalArrangement = Arrangement.spacedBy(16.dp),
                     contentPadding = contentPadding
                 ) {
-                    items(items = poster, key = { poster -> poster.id }) { poster ->
+                    items(items = poster) { poster ->
                         PosterHorizontal(
                             poster = poster,
                             animatedVisibilityScope = this@AnimatedContent,
@@ -70,7 +70,7 @@ fun TransitionBetweenColumnAndVerticalGrid(
                     horizontalArrangement = Arrangement.spacedBy(16.dp),
                     contentPadding = contentPadding
                 ) {
-                    items(items = poster, key = { poster -> poster.id }) { poster ->
+                    items(items = poster) { poster ->
                         PosterVertically(
                             poster = poster,
                             animatedVisibilityScope = this@AnimatedContent,
