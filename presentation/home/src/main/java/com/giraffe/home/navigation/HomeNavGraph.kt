13b7c5f91a6d42a6ef8c5b1327eac562437ee84f
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-08-01 01:54:17 +0300

    Refactor: Use `rememberSaveable` for `isBottomBarVisible`

--- presentation/home/src/main/java/com/giraffe/home/navigation/HomeNavGraph.kt
+++ presentation/home/src/main/java/com/giraffe/home/navigation/HomeNavGraph.kt
@@ -8,7 +8,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -74,7 +74,7 @@ fun HomeNavGraph(
         else -> null
     }
 
-    var isBottomBarVisible by remember {
+    var isBottomBarVisible by rememberSaveable {
         mutableStateOf(
             true
         )
