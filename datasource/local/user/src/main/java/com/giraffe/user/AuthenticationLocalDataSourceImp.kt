0f76586c7940a715e5eeb91218de6835265fe582
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-08-02 18:38:40 +0300

    rename exception in authentication and enhance the code

--- datasource/local/user/src/main/java/com/giraffe/user/AuthenticationLocalDataSourceImp.kt
+++ datasource/local/user/src/main/java/com/giraffe/user/AuthenticationLocalDataSourceImp.kt
@@ -1,23 +1,22 @@
 package com.giraffe.user
 
 import com.giraffe.repository.datasource.remote.AuthenticationLocalDataSource
+import com.giraffe.repository.utils.SafeCall
 import com.giraffe.user.datastore.AuthenticationDatastore
 import javax.inject.Inject
 
 class AuthenticationLocalDataSourceImp @Inject constructor(
     private val dataStore: AuthenticationDatastore
 ) : AuthenticationLocalDataSource {
-    override suspend fun saveSessionId(sessionId: String) {
+    override suspend fun saveSessionId(sessionId: String) = SafeCall {
         dataStore.saveSessionId(sessionId)
     }
 
-    override suspend fun getSessionId(): String? {
-        val sessionId = dataStore.getSessionId()
-        return sessionId
+    override suspend fun getSessionId(): String? = SafeCall {
+        dataStore.getSessionId()
     }
 
-    override suspend fun isLoggedIn(): Boolean {
-        val sessionId = dataStore.getSessionId()
-        return sessionId != null
+    override suspend fun isLoggedIn(): Boolean = SafeCall {
+        dataStore.getSessionId() != null
     }
 }
