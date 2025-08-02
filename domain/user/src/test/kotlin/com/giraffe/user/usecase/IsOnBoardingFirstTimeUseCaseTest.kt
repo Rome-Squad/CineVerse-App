89a8f743e1c95d76ace84617cedc2546fb17d7ef
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-08-02 19:16:03 +0300

    add test case for is onboardingFirstTime

---
+++ domain/user/src/test/kotlin/com/giraffe/user/usecase/IsOnBoardingFirstTimeUseCaseTest.kt
@@ -0,0 +1,40 @@
+package com.giraffe.user.usecase
+
+
+import com.giraffe.user.repository.OnboardingRepository
+import com.google.common.truth.Truth.assertThat
+import io.mockk.coEvery
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+
+class IsOnBoardingFirstTimeUseCaseTest {
+
+    private lateinit var onboardingRepository: OnboardingRepository
+    private lateinit var isOnBoardingFirstTimeUseCase: IsOnBoardingFirstTimeUseCase
+
+    @BeforeEach
+    fun setUp() {
+        onboardingRepository = mockk()
+        isOnBoardingFirstTimeUseCase = IsOnBoardingFirstTimeUseCase(onboardingRepository)
+    }
+
+    @Test
+    fun `should return true when first time, `() = runTest {
+        coEvery { onboardingRepository.isFirstTime() } returns true
+
+        val result = isOnBoardingFirstTimeUseCase()
+
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `should return false when not first time, `() = runTest {
+        coEvery { onboardingRepository.isFirstTime() } returns false
+
+        val result = isOnBoardingFirstTimeUseCase()
+
+        assertThat(result).isFalse()
+    }
+}
