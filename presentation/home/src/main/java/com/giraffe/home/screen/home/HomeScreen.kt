8dd0ace30d3c60180e25fd98635119dad945cd49
Ashraf-El-Fallah <elfallahashraf44@gmail.com>
2025-08-03 21:11:48 +0300

    remove un expected click when user click on top app bar and remove ripple effect

--- presentation/home/src/main/java/com/giraffe/home/screen/home/HomeScreen.kt
+++ presentation/home/src/main/java/com/giraffe/home/screen/home/HomeScreen.kt
@@ -2,6 +2,8 @@ package com.giraffe.home.screen.home
 
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -307,7 +309,11 @@ fun HomeContent(
                         topAppBarHeight = with(density) {
                             coordinates.size.height.toDp()
                         }
-                    },
+                    }
+                    .clickable(
+                        indication = null,
+                        interactionSource = remember { MutableInteractionSource() }
+                    ) {},
             ) {
                 TopAppBar(
                     userName = state.userName
