a14732caa7a1d9d54d91bd925b81d69448b11ec4
bilalazzam <blalazam065@gmail.com>
2025-08-03 18:07:48 +0300

    serialize CollectionDto

--- repository/media/src/main/java/com/giraffe/media/collections/datasource/remote/dto/CollectionDto.kt
+++ repository/media/src/main/java/com/giraffe/media/collections/datasource/remote/dto/CollectionDto.kt
@@ -1,11 +1,16 @@
 package com.giraffe.media.collections.datasource.remote.dto
 
-typealias CollectionTypeString = String
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
 
+typealias CollectionTypeString = String?
+
+@Serializable
 data class CollectionDto(
     val id: Int,
     val name: String,
     val description: String,
+    @SerialName("list_type")
     val type: CollectionTypeString = "movie"
 
 )
