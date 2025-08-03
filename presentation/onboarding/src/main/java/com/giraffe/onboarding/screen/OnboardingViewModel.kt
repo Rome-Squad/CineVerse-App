6077197c37e0bdf971c58e759362d4331e6da079
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-08-03 14:23:41 +0300

    create onboarding view model

---
+++ presentation/onboarding/src/main/java/com/giraffe/onboarding/screen/OnboardingViewModel.kt
@@ -0,0 +1,114 @@
+package com.giraffe.onboarding.screen
+
+import androidx.annotation.StringRes
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.giraffe.onboarding.R
+import com.giraffe.user.exception.UnknownException
+import com.giraffe.user.exception.UserException
+import com.giraffe.user.usecase.IsOnBoardingFirstTimeUseCase
+import com.giraffe.user.usecase.SetOnBoardingFirstTimeUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineExceptionHandler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class OnboardingViewModel @Inject constructor(
+    private val isOnBoardingFirstTimeUseCase: IsOnBoardingFirstTimeUseCase,
+    private val setOnBoardingFirstTimeUseCase: SetOnBoardingFirstTimeUseCase
+) : ViewModel() {
+    private val _state = MutableStateFlow(OnboardingUiState())
+    val state: StateFlow<OnboardingUiState> = _state.asStateFlow()
+
+    private val _effect = Channel<OnboardingEffect>()
+    val effect = _effect.receiveAsFlow()
+
+    fun onAction(action: OnboardingAction) {
+        when (action) {
+            OnboardingAction.CheckIfFirstTime -> checkIfFirstTime()
+            OnboardingAction.MarkOnboardingComplete -> markOnboardingComplete()
+        }
+    }
+
+    private fun checkIfFirstTime() {
+        safeExecute(
+            onSuccess = { result ->
+                _state.value = _state.value.copy(
+                    isFirstTime = result,
+                    isError = false
+                )
+            },
+            onError = { effect ->
+                _state.value = _state.value.copy(isError = true)
+                sendEffect(effect)
+            }
+        ) {
+            isOnBoardingFirstTimeUseCase()
+        }
+    }
+
+    private fun markOnboardingComplete() {
+        safeExecute(
+            onSuccess = {
+                _state.value = _state.value.copy(isError = false)
+                sendEffect(OnboardingEffect.NavigateToLogin)
+            },
+            onError = { effect ->
+                _state.value = _state.value.copy(isError = true)
+                sendEffect(effect)
+            }
+        ) {
+            setOnBoardingFirstTimeUseCase()
+        }
+    }
+
+    private fun <T> safeExecute(
+        coroutineScope: CoroutineScope = viewModelScope,
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        onSuccess: suspend (T) -> Unit,
+        onError: suspend (OnboardingEffect) -> Unit,
+        block: suspend CoroutineScope.() -> T
+    ): Job {
+        return coroutineScope.launch(dispatcher + handler(onError)) {
+            val result = block()
+            onSuccess(result)
+        }
+    }
+
+    private fun handler(onError: suspend (OnboardingEffect) -> Unit): CoroutineExceptionHandler {
+        return CoroutineExceptionHandler { _, throwable ->
+            if (throwable is UserException) {
+                val messageRes = mapExceptionToStringRes(throwable)
+                viewModelScope.launch {
+                    onError(OnboardingEffect.ShowError(messageRes))
+                }
+            }
+        }
+    }
+
+    private fun sendEffect(effect: OnboardingEffect) {
+        viewModelScope.launch {
+            _effect.send(effect)
+        }
+    }
+
+    @StringRes
+    private fun mapExceptionToStringRes(throwable: Throwable): Int {
+        return when (throwable) {
+            is AccessDeniedException -> R.string.error_access_denied
+            is UnknownException -> R.string.error_unknown
+            else -> R.string.error_unknown
+        }
+    }
+}
+
