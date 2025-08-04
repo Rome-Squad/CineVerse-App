a55d4946011950c3bc69bc8d9f39a0d9b13f436d
Hend Sayed <hendsayed853@gmail.com>
2025-08-04 19:46:06 +0300

    Refactor: Collect MainViewModel state in lifecycleScope

--- app/src/main/java/com/giraffe/cineverseapp/main/MainActivity.kt
+++ app/src/main/java/com/giraffe/cineverseapp/main/MainActivity.kt
@@ -11,11 +11,15 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.giraffe.authentication.AuthenticationApi
 import com.giraffe.designsystem.theme.CineVerseTheme
 import com.giraffe.home.HomeApi
 import dagger.hilt.android.AndroidEntryPoint
 import jakarta.inject.Inject
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -28,14 +32,19 @@ class MainActivity : ComponentActivity() {
     private val mainViewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
-        installSplashScreen().setKeepOnScreenCondition {
-            val state = mainViewModel.state.value
-            val notReady = state.isLoggedIn == null || state.isOnBoardingFirstTime == null
-            notReady
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                mainViewModel.state.collect { state ->
+                    splashScreen.setKeepOnScreenCondition {
+                        state.isLoggedIn == null || state.isOnBoardingFirstTime == null
+                    }
+                }
+            }
         }
-
 
         enableEdgeToEdge(
             statusBarStyle = SystemBarStyle.Companion.auto(
