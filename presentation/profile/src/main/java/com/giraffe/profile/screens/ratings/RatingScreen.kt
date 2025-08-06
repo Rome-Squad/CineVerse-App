89559717fc89331a3baf6bd10bb357f064c3fc9b
bilalazzam <blalazam065@gmail.com>
2025-08-06 19:46:56 +0300

    add padding to rating screen

--- presentation/profile/src/main/java/com/giraffe/profile/screens/ratings/RatingScreen.kt
+++ presentation/profile/src/main/java/com/giraffe/profile/screens/ratings/RatingScreen.kt
@@ -3,8 +3,10 @@ package com.giraffe.profile.screens.ratings
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.foundation.lazy.LazyColumn
