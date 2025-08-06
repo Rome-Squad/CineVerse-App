fa7a890da1d97ff9cf68a5007b1564b86f4eb3ad
bilalazzam <blalazam065@gmail.com>
2025-08-06 16:53:02 +0300

    add show error effect

--- presentation/profile/src/main/java/com/giraffe/profile/screens/ratings/RatingEffect.kt
+++ presentation/profile/src/main/java/com/giraffe/profile/screens/ratings/RatingEffect.kt
@@ -1,6 +1,11 @@
 package com.giraffe.profile.screens.ratings
 
+import androidx.annotation.StringRes
+
 sealed class RatingEffect {
+    data class ShowError(@param:StringRes val message: Int) : RatingEffect()
     data class NavigateToDetails(val id: Int) : RatingEffect()
     object NavigateBack : RatingEffect()
+
+
 }
