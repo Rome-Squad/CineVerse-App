15240f2770ab629540dd292e3599eb1e2a6ac670
pola george <86931053+poula22@users.noreply.github.com>
2025-08-07 18:11:12 +0300

    fix: - fix login button was not working

--- presentation/details/src/main/java/com/giraffe/details/screens/moviedetails/screen/MovieDetailsScreen.kt
+++ presentation/details/src/main/java/com/giraffe/details/screens/moviedetails/screen/MovieDetailsScreen.kt
@@ -304,7 +304,7 @@ private fun MovieDetailsContent(
 
         LoginBottomSheet(
             isVisible = state.isVisibleLoginBottomSheet,
-            onLogInClick = navigateToLogIn,
+            onLogInClick = interaction::onLoginButtonClick,
             onDismiss = interaction::onDismissLoginBottomSheet
         )
     }
