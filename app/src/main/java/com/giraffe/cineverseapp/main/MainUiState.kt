75de2383a096645d6ea49bbb142264016fac1497
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-08-07 23:35:44 +0300

    resolve launch app with english to launch with device launch

--- app/src/main/java/com/giraffe/cineverseapp/main/MainUiState.kt
+++ app/src/main/java/com/giraffe/cineverseapp/main/MainUiState.kt
@@ -1,8 +1,10 @@
 package com.giraffe.cineverseapp.main
 
+import java.util.Locale
+
 data class MainUiState(
     val isLoggedIn: Boolean? = null,
     val isOnBoardingFirstTime: Boolean? = null,
     val isDarkMode: Boolean = true,
-    val language: String = "en"
+    val language: String = Locale.getDefault().language
 )
