2aa3252ee2a46a0d31cc0baf793649de7bc64004
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-08-09 16:49:19 +0300

    add  get section title to show more section type and change name to show more.

--- presentation/home/src/main/java/com/giraffe/home/screen/show_more/ShowMoreRoute.kt
+++ presentation/home/src/main/java/com/giraffe/home/screen/show_more/ShowMoreRoute.kt
@@ -1,13 +1,14 @@
 package com.giraffe.home.screen.show_more
 
+import android.content.Context
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
+import com.giraffe.home.R
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class MoviesListRoute(
-    val collectionId: Int,
+data class ShowMoreRoute(
     val sectionType: ShowMoreSectionType
 )
 
@@ -19,33 +20,33 @@ enum class ShowMoreSectionType {
     RECENTLY_VIEWED,
     MATCHES_YOUR_VIBES;
 
-    fun isPredefinedSection(): Boolean {
+    fun getSectionTitle(context: Context): String {
         return when (this) {
-            RECENTLY_RELEASED,
-            TOP_RATED_TV_SHOWS,
-            UPCOMING_MOVIES,
-            RECENTLY_VIEWED,
-            MATCHES_YOUR_VIBES -> true
+            RECENTLY_RELEASED -> context.getString(R.string.recently_released)
+            TOP_RATED_TV_SHOWS -> context.getString(R.string.top_rated_tv_shows)
+            UPCOMING_MOVIES -> context.getString(R.string.upcoming_movies)
+            RECENTLY_VIEWED -> context.getString(R.string.you_recent_viewed)
+            MATCHES_YOUR_VIBES -> context.getString(R.string.matches_your_vibe)
         }
     }
+
 }
 
-fun NavController.navigateToMoviesList(sectionType: ShowMoreSectionType, collectionId: String) {
+fun NavController.navigateToShowMore(sectionType: ShowMoreSectionType) {
     navigate(
-        MoviesListRoute(
+        ShowMoreRoute(
             sectionType = sectionType,
-            collectionId =5
         )
     )
 }
 
-fun NavGraphBuilder.moviesListRoute(
+fun NavGraphBuilder.showMoreRoute(
     onBackClick: () -> Unit,
     navigateToMoviesDetailsScreen: (Int) -> Unit,
     navigateToSeriesDetailsScreen: (Int) -> Unit,
 ) {
-    composable<MoviesListRoute> {
-        MoviesListScreen(
+    composable<ShowMoreRoute> {
+        ShowMoreScreen(
             onBackClick = onBackClick,
             navigateToMoviesDetailsScreen = navigateToMoviesDetailsScreen,
             navigateToSeriesDetailsScreen = navigateToSeriesDetailsScreen,
