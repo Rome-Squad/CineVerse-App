ed007169a27713885d7ec0fbb9fc405afd05ea3f
Kareem Mohamed <63722@fa-hiss.edu.eg>
2025-08-09 14:02:40 +0300

    add presentation module in di for show more strategy.

---
+++ app/src/main/java/com/giraffe/cineverseapp/di/presentationModule.kt
@@ -0,0 +1,93 @@
+package com.giraffe.cineverseapp.di
+
+import com.giraffe.home.screen.show_more.MatchesYourVibesStrategy
+import com.giraffe.home.screen.show_more.RecentlyReleasedStrategy
+import com.giraffe.home.screen.show_more.RecentlyViewedStrategy
+import com.giraffe.home.screen.show_more.ShowMoreFactory
+import com.giraffe.home.screen.show_more.TopRatedTvShowsStrategy
+import com.giraffe.home.screen.show_more.UpcomingMoviesStrategy
+import com.giraffe.media.movies.usecase.GetRecentlyReleasedMoviesUseCase
+import com.giraffe.media.movies.usecase.GetRecentlyViewedMoviesUseCase
+import com.giraffe.media.movies.usecase.GetRecommendedMovieUseCase
+import com.giraffe.media.movies.usecase.GetUpcomingMoviesUseCase
+import com.giraffe.media.series.usecase.GetRecentSeriesUseCase
+import com.giraffe.media.series.usecase.GetRecentlyReleasedSeriesUseCase
+import com.giraffe.media.series.usecase.GetRecommendedSeriesUseCase
+import com.giraffe.media.series.usecase.GetTopRatedSeriesUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object PresentationModule {
+    @Provides
+    @Singleton
+    fun provideRecentlyReleasedStrategy(
+        getRecentlyReleasedMovies: GetRecentlyReleasedMoviesUseCase,
+        getRecentlyReleasedSeries: GetRecentlyReleasedSeriesUseCase
+    ): RecentlyReleasedStrategy {
+        return RecentlyReleasedStrategy(getRecentlyReleasedMovies, getRecentlyReleasedSeries)
+    }
+
+    @Provides
+    @Singleton
+    fun provideTopRatedTvShowsStrategy(
+        getTopRatedSeries: GetTopRatedSeriesUseCase
+    ): TopRatedTvShowsStrategy {
+        return TopRatedTvShowsStrategy(getTopRatedSeries)
+    }
+
+    @Provides
+    @Singleton
+    fun provideUpcomingMoviesStrategy(
+        getUpcomingMovies: GetUpcomingMoviesUseCase
+    ): UpcomingMoviesStrategy {
+        return UpcomingMoviesStrategy(getUpcomingMovies)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRecentlyViewedStrategy(
+        getRecentlyViewedMovies: GetRecentlyViewedMoviesUseCase,
+        getRecentlySeriesUseCase: GetRecentSeriesUseCase
+    ): RecentlyViewedStrategy {
+        return RecentlyViewedStrategy(getRecentlyViewedMovies, getRecentlySeriesUseCase)
+    }
+
+    @Provides
+    @Singleton
+    fun provideMatchesYourVibesStrategy(
+        getRecentlyViewedMovies: GetRecentlyViewedMoviesUseCase,
+        getRecentlySeriesUseCase: GetRecentSeriesUseCase,
+        getRecommendedMovie: GetRecommendedMovieUseCase,
+        getRecommendedSeries: GetRecommendedSeriesUseCase
+    ): MatchesYourVibesStrategy {
+        return MatchesYourVibesStrategy(
+            getRecentlyViewedMovies,
+            getRecentlySeriesUseCase,
+            getRecommendedMovie,
+            getRecommendedSeries
+        )
+    }
+
+    @Provides
+    @Singleton
+    fun provideShowMoreFactory(
+        recentlyReleasedStrategy: RecentlyReleasedStrategy,
+        topRatedTvShowsStrategy: TopRatedTvShowsStrategy,
+        upcomingMoviesStrategy: UpcomingMoviesStrategy,
+        recentlyViewedStrategy: RecentlyViewedStrategy,
+        matchesYourVibesStrategy: MatchesYourVibesStrategy
+    ): ShowMoreFactory {
+        return ShowMoreFactory(
+            recentlyReleasedStrategy,
+            topRatedTvShowsStrategy,
+            upcomingMoviesStrategy,
+            recentlyViewedStrategy,
+            matchesYourVibesStrategy
+        )
+    }
+}
