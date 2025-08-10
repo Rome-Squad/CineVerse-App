967032f330df8701da151bff62de84512c840d5d
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-08-10 21:19:37 +0300

    fix conflict in navigation

--- presentation/match/src/main/java/com/giraffe/match/navigation/MatchApiImp.kt
+++ presentation/match/src/main/java/com/giraffe/match/navigation/MatchApiImp.kt
@@ -2,10 +2,8 @@ package com.giraffe.match.navigation
 
 import androidx.compose.runtime.Composable
 import com.giraffe.match.MatchApi
-import com.giraffe.match.screen.MatchScreen
 import androidx.navigation.compose.rememberNavController
 import com.giraffe.api.details.DetailsApi
-import com.giraffe.match.MatchApi
 import javax.inject.Inject
 
 class MatchApiImp @Inject constructor(
