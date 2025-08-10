aaf9be7364f39168f327f016224a4b6c73f34e68
Hend Sayed <hendsayed853@gmail.com>
2025-08-10 23:31:22 +0300

    Refactor: Relocate profile navigation routes

--- presentation/profile/src/main/java/com/giraffe/presentation/profile/navigation/ProfileNavGraph.kt
+++ presentation/profile/src/main/java/com/giraffe/presentation/profile/navigation/ProfileNavGraph.kt
@@ -13,17 +13,26 @@ import androidx.navigation.toRoute
 import com.giraffe.api.authentication.AuthenticationApi
 import com.giraffe.api.details.DetailsApi
 import com.giraffe.api.explore.ExploreApi
-import com.giraffe.presentation.profile.edit.editProfileWebViewRoute
-import com.giraffe.presentation.profile.screens.collections.collection.collectionRoute
-import com.giraffe.presentation.profile.screens.collections.collection.navigateToCollection
-import com.giraffe.presentation.profile.screens.collections.mycollections.myCollectionsRoute
-import com.giraffe.presentation.profile.screens.collections.mycollections.navigateToMyCollections
-import com.giraffe.presentation.profile.screens.history.historyRoute
-import com.giraffe.presentation.profile.screens.history.navigateToHistory
-import com.giraffe.presentation.profile.screens.ratings.navigateToRatings
-import com.giraffe.presentation.profile.screens.ratings.ratingsRoute
-import com.giraffe.presentation.profile.screens.settings.SettingsScreenRoute
-import com.giraffe.presentation.profile.screens.settings.settingsScreenRoute
+import com.giraffe.presentation.profile.navigation.routes.ExploreRoute
+import com.giraffe.presentation.profile.navigation.routes.MovieDetailsRoute
+import com.giraffe.presentation.profile.navigation.routes.SeriesDetailsRoute
+import com.giraffe.presentation.profile.navigation.routes.SettingsScreenRoute
+import com.giraffe.presentation.profile.navigation.routes.collectionRoute
+import com.giraffe.presentation.profile.navigation.routes.historyRoute
+import com.giraffe.presentation.profile.navigation.routes.loginRoute
+import com.giraffe.presentation.profile.navigation.routes.myCollectionsRoute
+import com.giraffe.presentation.profile.navigation.routes.navigateLoginScreen
+import com.giraffe.presentation.profile.navigation.routes.navigateToCollection
+import com.giraffe.presentation.profile.navigation.routes.navigateToExploreScreen
+import com.giraffe.presentation.profile.navigation.routes.navigateToHistory
+import com.giraffe.presentation.profile.navigation.routes.navigateToMovieDetails
+import com.giraffe.presentation.profile.navigation.routes.navigateToMyCollections
+import com.giraffe.presentation.profile.navigation.routes.navigateToRatings
+import com.giraffe.presentation.profile.navigation.routes.navigateToSeriesDetails
+import com.giraffe.presentation.profile.navigation.routes.ratingsRoute
+import com.giraffe.presentation.profile.navigation.routes.settingsScreenRoute
+import editProfileWebViewRoute
+
 
 @Composable
 internal fun ProfileNavGraph(
