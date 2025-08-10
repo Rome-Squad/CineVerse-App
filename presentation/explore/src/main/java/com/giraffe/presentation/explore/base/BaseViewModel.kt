f708af4e2cd825bc79e0fd5f1f6e877d7dfb3ef6
Hend Sayed <hendsayed853@gmail.com>
2025-08-10 12:38:26 +0300

    Update BaseViewModel to include effects

---
+++ presentation/explore/src/main/java/com/giraffe/presentation/explore/base/BaseViewModel.kt
@@ -0,0 +1,69 @@
+package com.giraffe.presentation.explore.base
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+
+
+abstract class BaseViewModel<S, E>(initialState: S) : ViewModel() {
+    private val _state = MutableStateFlow(initialState)
+    val state = _state.asStateFlow()
+
+    private val _effect = Channel<E>()
+    val effect = _effect.receiveAsFlow()
+
+    protected fun <T> safeExecute(
+        coroutineScope: CoroutineScope = viewModelScope,
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        onSuccess: suspend (T) -> Unit = {},
+        onError: (Throwable) -> Unit = {},
+        block: suspend CoroutineScope.() -> T
+    ) = coroutineScope.launch(dispatcher) {
+        try {
+            onSuccess(block())
+        } catch (e: Throwable) {
+            onError(e)
+        }
+    }
+
+    protected fun <T> safeCollect(
+        onError: (Throwable) -> Unit = {},
+        onEmitNewValue: (T) -> Unit = {},
+        coroutineScope: CoroutineScope = viewModelScope,
+        dispatcher: CoroutineDispatcher = Dispatchers.IO,
+        block: suspend () -> Flow<T>
+    ) {
+        coroutineScope.launch(dispatcher) {
+            block()
+                .catch {
+                    onError(it)
+                }.collect {
+                    onEmitNewValue(it)
+                }
+        }
+    }
+
+    protected fun updateState(updater: (S) -> S) {
+        _state.update(updater)
+    }
+
+    protected fun sendEffect(
+        effect: E,
+        coroutineScope: CoroutineScope = viewModelScope,
+        dispatcher: CoroutineDispatcher = Dispatchers.Main,
+    ) {
+        coroutineScope.launch(dispatcher) {
+            _effect.send(effect)
+        }
+    }
+}
