1331d188d009df43d03bf1de66529091b2682e07
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-08-10 17:41:46 +0300

    create match screen

--- presentation/match/src/main/java/com/giraffe/match/navigation/MatchApiImp.kt
+++ presentation/match/src/main/java/com/giraffe/match/navigation/MatchApiImp.kt
@@ -1,14 +1,8 @@
 package com.giraffe.match.navigation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import com.giraffe.designsystem.composable.custom.Text
 import com.giraffe.match.MatchApi
-import com.giraffe.match.R
+import com.giraffe.match.screen.MatchScreen
 import javax.inject.Inject
 
 class MatchApiImp @Inject constructor(): MatchApi {
@@ -18,24 +12,5 @@ class MatchApiImp @Inject constructor(): MatchApi {
         MatchScreen(
             onShowBottomBarChange = onShowBottomBarChange
         )
-    }
-
-    @Composable
-    fun MatchScreen(
-        modifier: Modifier = Modifier,
-        onShowBottomBarChange: (Boolean) -> Unit
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-        ) {
-            Text(
-                modifier = Modifier
-                    .align(Alignment.Center),
-                text = stringResource(
-                    id = R.string.match_screen
-                ),
-            )
-        }
     }
 }
