95c596d58eeaae775776c94b1d3eea30021b8fce
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-08-11 02:55:30 +0300

    update test cases and create dummy file

--- domain/media/src/test/kotlin/com/giraffe/media/explore/usecase/ClearSearchHistoryUseCaseTest.kt
+++ domain/media/src/test/kotlin/com/giraffe/media/explore/usecase/ClearSearchHistoryUseCaseTest.kt
@@ -1,29 +1,21 @@
 package com.giraffe.media.explore.usecase
 
-import com.giraffe.media.explore.repository.ExploreRepository
+import com.giraffe.media.explore.repository.SearchRepository
 import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.test.runTest
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
 class ClearSearchHistoryUseCaseTest {
 
-    private lateinit var repository: ExploreRepository
-    private lateinit var useCase: ClearSearchHistoryUseCase
-
-    @BeforeEach
-    fun setUp() {
-        repository = mockk(relaxed = true)
-        useCase = ClearSearchHistoryUseCase(repository)
-    }
+    private var repository: SearchRepository = mockk(relaxed = true)
+    private var useCase: ClearSearchHistoryUseCase = ClearSearchHistoryUseCase(repository)
 
     @Test
     fun `should call repository to clear search history when execute is invoked`() = runTest {
-        // When
-        useCase()
 
-        // Then
+    useCase()
+
         coVerify { repository.clearSearchHistory() }
     }
 }
