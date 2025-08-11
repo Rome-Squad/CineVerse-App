49c4348254599fd099df9191c4a40cc3e0b41d24
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-08-11 03:45:14 +0300

    create file for dummy data

---
+++ domain/user/src/test/kotlin/com/giraffe/user/dummydata/DummyData.kt
@@ -0,0 +1,10 @@
+package com.giraffe.user.dummydata
+
+import com.giraffe.user.entity.User
+
+val fakeUser = User(
+    id = 1,
+    displayName = "hamada rayyan",
+    username = "hamada_rayyan",
+    avatarUrl = "https://example.com/avatar.jpg"
+)
