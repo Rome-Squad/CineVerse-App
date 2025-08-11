f5c3e923a72f9b6d66b4696571680ed713104993
Hend Sayed <hendsayed853@gmail.com>
2025-08-11 10:56:36 +0300

    Add showToast extension function for Context

--- presentation/explore/src/main/java/com/giraffe/presentation/explore/util/Extention.kt
+++ presentation/explore/src/main/java/com/giraffe/presentation/explore/util/Extention.kt
@@ -1,6 +1,8 @@
 package com.giraffe.presentation.explore.util
 
 import android.content.Context
+import android.widget.Toast
+import androidx.annotation.StringRes
 import com.giraffe.presentation.explore.R
 import com.giraffe.presentation.explore.screen.discover.SearchTab
 
@@ -8,4 +10,8 @@ fun SearchTab.toTitle(context: Context) = when (this) {
     SearchTab.MOVIES -> context.getString(R.string.movies)
     SearchTab.SERIES -> context.getString(R.string.series)
     SearchTab.ACTORS -> context.getString(R.string.actors)
+}
+
+fun Context.showToast(@StringRes stringRes: Int, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(this, this.getString(stringRes), duration).show()
 }
