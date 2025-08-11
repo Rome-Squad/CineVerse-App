a1db4fb04ebd780070c070ac133d9b6aa3996e51
Mohamed Skaik <mohamedsaeb.skaik@gmail.com>
2025-08-11 12:02:36 +0300

    Refactor: Improve null safety in ConvertFormatted.kt
    This commit refactors the `toFormattedDuration` and `toFormattedDate` functions in `ConvertFormatted.kt` to improve null safety by using the safe call operator (`?.let`).
    
    The `toFormattedDuration` function now returns an empty string if the input integer is null or less than or equal to zero.
    The `toFormattedDate` function now returns an empty string if the input `LocalDate` is null.

--- presentation/details/src/main/java/com/giraffe/presentation/details/utils/ConvertFormatted.kt
+++ presentation/details/src/main/java/com/giraffe/presentation/details/utils/ConvertFormatted.kt
@@ -5,22 +5,24 @@ import java.text.SimpleDateFormat
 import java.util.Locale
 
 fun Int?.toFormattedDuration(): String {
-    if (this == null || this < 0) return ""
-    val hours = this / 60
-    val minutes = this % 60
-    return buildString {
-        if (hours > 0) append("${hours}h ")
-        if (minutes > 0 || hours == 0) append("${minutes}m")
-    }.trim()
+    return this?.let {
+        if (it <= 0) return ""
+        val hours = it / 60
+        val minutes = it % 60
+        return buildString {
+            if (hours > 0) append("${hours}h ")
+            if (minutes > 0 || hours == 0) append("${minutes}m")
+        }.trim()
+    } ?: ""
 }
 
 fun LocalDate?.toFormattedDate(): String {
-    if (this == null) return ""
+    return this?.let {
+        val dateString = it.toString()
+        val inputFormat = SimpleDateFormat("yyyy-M-dd", Locale.ENGLISH)
+        val date = inputFormat.parse(dateString) ?: return dateString
 
-    val dateString = this.toString()
-    val inputFormat = SimpleDateFormat("yyyy-M-dd", Locale.ENGLISH)
-    val date = inputFormat.parse(dateString) ?: return dateString
-
-    val outputFormat = SimpleDateFormat("yyyy, MMM d", Locale.ENGLISH)
-    return outputFormat.format(date)
+        val outputFormat = SimpleDateFormat("yyyy, MMM d", Locale.ENGLISH)
+        return outputFormat.format(date)
+    } ?: ""
 }
