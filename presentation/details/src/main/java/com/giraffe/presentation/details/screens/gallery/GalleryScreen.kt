72acba3ff3ec0cd0c31bd9d725e190ae117391dd
bilalazzam <blalazam065@gmail.com>
2025-08-11 12:41:40 +0300

    add interactions and effects to gallary screen

--- presentation/details/src/main/java/com/giraffe/presentation/details/screens/gallery/GalleryScreen.kt
+++ presentation/details/src/main/java/com/giraffe/presentation/details/screens/gallery/GalleryScreen.kt
@@ -1,123 +1,87 @@
 package com.giraffe.presentation.details.screens.gallery
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.giraffe.designsystem.composable.AppBar
-import com.giraffe.designsystem.composable.custom.Text
-import com.giraffe.designsystem.theme.Theme
 import com.giraffe.presentation.details.R
-import com.giraffe.presentation.details.components.LoadingView
+import com.giraffe.presentation.details.base.BaseScreen
 import com.giraffe.presentation.details.components.gallery.GalleryItemLayoutLTR
 import com.giraffe.presentation.details.components.gallery.GalleryItemLayoutRTL
+import com.giraffe.presentation.details.utils.EventListener
+import com.giraffe.presentation.details.utils.showToast
+import com.giraffe.presentation.details.utils.toStringResource
 
 @Composable
 fun GalleryScreen(
     onBackClick: () -> Unit,
-    modifier: Modifier = Modifier,
     galleryViewModel: GalleryViewModel = hiltViewModel()
 ) {
     val state by galleryViewModel.state.collectAsState()
-    if (state.isLoading) {
-        LoadingView()
-    } else {
-        Box(
-            modifier = modifier
-                .fillMaxSize()
-                .background(Theme.color.background.screen)
-                .wrapContentSize()
-        ) {
-            GalleryContent(
-                state = state,
-                onBackArrowClick = onBackClick,
-                modifier = modifier
+    val context = LocalContext.current
+
+    EventListener(
+        events = galleryViewModel.effect
+    ) { effect ->
+        when (effect) {
+            is GalleryEffect.ShowError -> context.showToast(
+                effect.error.toStringResource()
             )
-            AnimatedVisibility(
-                visible = !state.errorMessage.isNullOrBlank(),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .navigationBarsPadding()
-                    .background(Theme.color.shade.primary)
-                    .padding(horizontal = 16.dp, vertical = 8.dp)
-                    .align(Alignment.BottomCenter)
-            ) {
-                Text(
-                    text = state.errorMessage
-                        ?: stringResource(R.string.unknown_error),
-                    color = Theme.color.additional.primary.red,
-                    textAlign = TextAlign.Center,
-                    style = Theme.textStyle.label.md.regular
-                )
-            }
+
+            GalleryEffect.NavigateBack -> onBackClick()
         }
+
     }
+
+    GalleryContent(
+        state = state,
+        interaction = galleryViewModel,
+    )
 }
 
 @Composable
 fun GalleryContent(
     state: GalleryUiState,
-    onBackArrowClick: () -> Unit,
-    modifier: Modifier = Modifier,
+    interaction: GalleryInteractionListener,
 ) {
-    Column(
-        modifier = modifier
-            .background(Theme.color.background.screen)
-            .systemBarsPadding()
+    BaseScreen(
+        title = state.actorName + " " + stringResource(R.string.gallery),
+        isLoading = state.isLoading,
+        isNoInternet = state.isNoInternet,
+        onBackClick = interaction::onBackClick,
     ) {
-        AppBar(
-            title = state.actorName + " " + stringResource(R.string.gallery),
-            showBackButton = true,
-            hasBackground = false,
-            onBackButtonClick = onBackArrowClick,
-            modifier = Modifier.padding(horizontal = 8.dp)
-        )
-        ImageGalleryLayout(imageUrls = state.imageUrls)
-    }
-}
-
-@Composable
-fun ImageGalleryLayout(imageUrls: List<String?>) {
-    LazyColumn(
-        contentPadding = PaddingValues(vertical = 10.dp, horizontal = 16.dp),
-        verticalArrangement = Arrangement.Top,
-        modifier = Modifier.fillMaxSize(),
-    ) {
-        items(imageUrls.chunked(6)) { subImageUrls ->
-            if (subImageUrls.take(3).isNotEmpty()) {
-                GalleryItemLayoutLTR(
-                    imageUrls = subImageUrls.take(3),
-                    modifier = Modifier
-                        .height(280.dp)
-                        .padding(top = 6.dp, bottom = 6.dp),
-                )
-                if (subImageUrls.size > 3) {
-                    GalleryItemLayoutRTL(
-                        imageUrls = subImageUrls.subList(3, subImageUrls.size),
+        LazyColumn(
+            contentPadding = PaddingValues(vertical = 10.dp, horizontal = 16.dp),
+            verticalArrangement = Arrangement.Top,
+            modifier = Modifier.fillMaxSize(),
+        ) {
+            items(state.imageUrls.chunked(6)) { subImageUrls ->
+                if (subImageUrls.take(3).isNotEmpty()) {
+                    GalleryItemLayoutLTR(
+                        imageUrls = subImageUrls.take(3),
                         modifier = Modifier
                             .height(280.dp)
                             .padding(top = 6.dp, bottom = 6.dp),
                     )
+                    if (subImageUrls.size > 3) {
+                        GalleryItemLayoutRTL(
+                            imageUrls = subImageUrls.subList(3, subImageUrls.size),
+                            modifier = Modifier
+                                .height(280.dp)
+                                .padding(top = 6.dp, bottom = 6.dp),
+                        )
+                    }
                 }
             }
         }
