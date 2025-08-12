04abcf883329482c633ec5e789d39b798b7a8c73
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-08-12 19:38:09 +0300

    create safe call instead of retrofit request builder

---
+++ datasource/remote/user/src/main/java/com/giraffe/user/util/SafeCall.kt
@@ -0,0 +1,34 @@
+package com.giraffe.user.util
+
+import com.giraffe.repository.exceptions.ApiDataException
+import com.giraffe.repository.exceptions.InvalidIdDataException
+import com.giraffe.repository.exceptions.NoInternetDataException
+import com.giraffe.repository.exceptions.RequestTimeoutDataException
+import com.giraffe.repository.exceptions.SerializationDataException
+import com.giraffe.repository.exceptions.UnknownNetworkDataException
+import com.giraffe.repository.exceptions.UserDataException
+import retrofit2.Response
+import java.io.IOException
+import java.net.SocketTimeoutException
+
+
+suspend fun <T> safeCall(execute: suspend () -> Response<T>): T {
+    return try {
+        val response = execute()
+        if (response.isSuccessful) {
+            response.body() ?: throw SerializationDataException()
+        } else {
+            throw ApiDataException(response.code())
+        }
+    } catch (e: Throwable) {
+        throw mapToMediaException(e)
+    }
+}
+
+fun mapToMediaException(e: Throwable): UserDataException = when (e) {
+    is UserDataException -> e
+    is SocketTimeoutException -> RequestTimeoutDataException()
+    is IOException -> NoInternetDataException()
+    is IllegalArgumentException -> InvalidIdDataException()
+    else -> UnknownNetworkDataException()
+}
