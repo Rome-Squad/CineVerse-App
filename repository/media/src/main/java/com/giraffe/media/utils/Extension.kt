da9e851866ae260f71e14570b72b5a189284ce33
Mohamed Ashraf <ma1261327@gmail.com>
2025-08-12 23:16:16 +0300

    show image popular series in home screen

--- repository/media/src/main/java/com/giraffe/media/utils/Extension.kt
+++ repository/media/src/main/java/com/giraffe/media/utils/Extension.kt
@@ -1,16 +1,5 @@
 package com.giraffe.media.utils
 
-import kotlinx.datetime.LocalDateTime
-import kotlinx.datetime.TimeZone
-import kotlinx.datetime.toLocalDateTime
-import kotlin.time.Clock
-import kotlin.time.ExperimentalTime
-
-@OptIn(ExperimentalTime::class)
-fun getCurrentLocalDateTime(): LocalDateTime {
-    return Clock.System.now().toLocalDateTime(TimeZone.currentSystemDefault())
-}
-
 fun Float?.orEmpty() = this ?: 0f
 
 fun Any?.orEmpty(): String = this?.toString() ?: ""
