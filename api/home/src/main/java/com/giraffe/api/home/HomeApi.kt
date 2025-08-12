ba94778372837ef9309ca500a5ce50946bcd38e3
bilalazzam <blalazam065@gmail.com>
2025-08-12 12:52:12 +0300

    fix conflicting destinations with actual screen being presented

--- api/home/src/main/java/com/giraffe/api/home/HomeApi.kt
+++ api/home/src/main/java/com/giraffe/api/home/HomeApi.kt
@@ -4,6 +4,8 @@ package com.giraffe.api.home
 import androidx.compose.runtime.Composable
 
 interface HomeApi {
+
+    fun navigateToExploreScreen()
     @Composable
     fun MainContainer()
 }
--- presentation/home/src/main/java/com/giraffe/presentation/home/navigation/HomeApiImp.kt
+++ presentation/home/src/main/java/com/giraffe/presentation/home/navigation/HomeApiImp.kt
@@ -9,23 +9,32 @@ import com.giraffe.api.home.HomeApi
 import com.giraffe.api.profile.ProfileApi
 import com.giraffe.match.MatchApi
 import com.giraffe.presentation.home.navigation.main.MainNavGraph
+import com.giraffe.presentation.home.navigation.main.routes.navigateToExplore
 import javax.inject.Inject
+import javax.inject.Provider
 
 class HomeApiImp @Inject constructor(
     private val detailsApi: DetailsApi,
     private val exploreApi: ExploreApi,
-    private val profileApi: ProfileApi,
+    private val profileApi: Provider<ProfileApi>,
     private val matchApi: MatchApi
 ) : HomeApi {
+    private lateinit var navController: NavHostController
+
     @Composable
     override fun MainContainer() {
-        val navController: NavHostController = rememberNavController()
+        navController = rememberNavController()
+
         MainNavGraph(
             navController = navController,
             detailsApi = detailsApi,
             exploreApi = exploreApi,
-            profileApi = profileApi,
+            profileApi = profileApi.get(),
             matchApi = matchApi
         )
+    }
+
+    override fun navigateToExploreScreen() {
+        navController.navigateToExplore()
     }
 }
