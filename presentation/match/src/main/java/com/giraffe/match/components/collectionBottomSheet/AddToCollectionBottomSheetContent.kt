5e9efad2c44c7fc95afc7f72225f47ac62d3af88
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-08-13 05:11:51 +0300

    add login and collection bottom sheets and components

---
+++ presentation/match/src/main/java/com/giraffe/match/components/collectionBottomSheet/AddToCollectionBottomSheetContent.kt
@@ -0,0 +1,31 @@
+package com.giraffe.match.components.collectionBottomSheet
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.giraffe.match.screen.result.CollectionUi
+
+@Composable
+fun AddToCollectionBottomSheetContent(
+    collectionsList: List<CollectionUi>,
+    onCollectionClick: (Int) -> Unit,
+    onNewCollectionClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        contentPadding = PaddingValues(top = 20.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        itemsIndexed(collectionsList) { index, collection ->
+            AddToCollectionItem(
+                title = collection.title,
+                isLoading = collection.isLoading,
+                onCollectionClicked = { onCollectionClick(collection.id) }
+            )
+        }
+    }
+}
