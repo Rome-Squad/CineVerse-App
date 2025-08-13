3d5443eaca48068e6a564695ab16c755226b704f
bilalazzam <blalazam065@gmail.com>
2025-08-13 11:05:21 +0300

    create CineVerseRoot

--- app/src/main/java/com/giraffe/cineverseapp/main/MainActivity.kt
+++ app/src/main/java/com/giraffe/cineverseapp/main/MainActivity.kt
@@ -1,24 +1,17 @@
 package com.giraffe.cineverseapp.main
 
 import android.os.Bundle
-import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.toArgb
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
 import com.giraffe.api.authentication.AuthenticationApi
-import com.giraffe.designsystem.theme.CineVerseTheme
-import com.giraffe.presentation.profile.utils.LanguageHelper
-
 import dagger.hilt.android.AndroidEntryPoint
 import jakarta.inject.Inject
 import kotlinx.coroutines.flow.collectLatest
@@ -49,34 +42,14 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-        enableEdgeToEdge(
-            statusBarStyle = SystemBarStyle.Companion.auto(
-                Color.Companion.Transparent.toArgb(),
-                Color.Companion.Transparent.toArgb()
-            ),
-            navigationBarStyle = SystemBarStyle.Companion.auto(
-                Color.Companion.Transparent.toArgb(),
-                Color.Companion.Transparent.toArgb()
-            )
-        )
 
         setContent {
             val state by mainViewModel.state.collectAsState()
-
-            CineVerseTheme(
-                isDarkTheme = state.isDarkMode
-            ) {
-
-                LaunchedEffect(state.language) {
-                    LanguageHelper.updateAppLocale(state.language)
-                }
-
-                authenticationApi.LoginContainer(
-                    onBack = {},
-                    isOnboardingFirstTime = state.isOnBoardingFirstTime == true,
-                    isLoggedIn = state.isLoggedIn == true
-                )
-            }
+            CineVerseRoot(
+                enableEdgeToEdge = ::enableEdgeToEdge,
+                state = state,
+                authenticationApi = authenticationApi
+            )
         }
     }
 }
