be34e8aec720e98625992ee14131e8dd2a3c4b5c
bilalazzam <blalazam065@gmail.com>
2025-08-13 17:41:15 +0300

    fix recommended screens padding

--- presentation/details/src/main/java/com/giraffe/presentation/details/screens/videoPlayer/YouTubePlayerScreen.kt
+++ presentation/details/src/main/java/com/giraffe/presentation/details/screens/videoPlayer/YouTubePlayerScreen.kt
@@ -1,10 +1,8 @@
 package com.giraffe.presentation.details.screens.videoPlayer
 
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
