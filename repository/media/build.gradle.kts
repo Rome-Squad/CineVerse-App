8d4bfa23bff8c0eb66a720e5fc3593133ef09cc1
pola george <86931053+poula22@users.noreply.github.com>
2025-08-14 23:49:41 +0300

    feature: use custom gradle plugin in repository modules :repository:media and :repository:user

--- repository/media/build.gradle.kts
+++ repository/media/build.gradle.kts
@@ -1,37 +1,13 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.cineVerse.android.library)
     alias(libs.plugins.ksp)
     alias(libs.plugins.kotlin.serialization)
 }
 
 android {
     namespace = "com.giraffe.media"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
@@ -44,7 +20,6 @@ dependencies {
     ksp(libs.room.compiler)
     implementation(libs.kotlinx.serialization.json)
     annotationProcessor(libs.room.compiler)
-    testImplementation(libs.bundles.unit.test)
     // Hilt DI
     implementation(libs.hilt.android)
     ksp(libs.hilt.compiler)
--- repository/user/build.gradle.kts
+++ repository/user/build.gradle.kts
@@ -1,42 +1,17 @@
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.cineVerse.android.library)
     alias(libs.plugins.kotlin.serialization)
     alias(libs.plugins.ksp)
 }
 
 android {
     namespace = "com.giraffe.repository"
-    compileSdk = 36
-
-    defaultConfig {
-        minSdk = 24
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        consumerProguardFiles("consumer-rules.pro")
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
     implementation(projects.domain.user)
-    testImplementation(libs.bundles.unit.test)
     implementation(libs.kotlinx.serialization.json)
     // Hilt DI
     implementation(libs.hilt.android)
