dd8c687e4bc9970f8a11abc6e09d6c09e6af98ac
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-08-14 17:33:14 +0300

    restricted web view side urls

--- presentation/profile/src/main/java/com/giraffe/presentation/profile/screens/edit/EditProfileWebViewScreen.kt
+++ presentation/profile/src/main/java/com/giraffe/presentation/profile/screens/edit/EditProfileWebViewScreen.kt
@@ -1,6 +1,8 @@
 package com.giraffe.presentation.profile.screens.edit
 
 import android.graphics.Color
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -8,6 +10,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.systemBarsPadding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
@@ -16,6 +19,7 @@ import com.giraffe.designsystem.composable.AppBar
 import com.giraffe.designsystem.composable.Progress
 import com.giraffe.designsystem.theme.Theme
 import com.giraffe.presentation.profile.R
+import com.google.accompanist.web.AccompanistWebViewClient
 import com.google.accompanist.web.LoadingState
 import com.google.accompanist.web.WebView
 import com.google.accompanist.web.rememberWebViewState
@@ -26,6 +30,7 @@ private const val EDIT_PROFILE_URL = "https://www.themoviedb.org/settings/profil
 fun EditProfileWebViewScreen(onBack: () -> Unit) {
     val webViewState = rememberWebViewState(url = EDIT_PROFILE_URL)
     val isLoading = webViewState.loadingState is LoadingState.Loading
+    val webViewClient = remember { RestrictedWebViewClient() }
 
     Box(
         modifier = Modifier
@@ -42,6 +47,7 @@ fun EditProfileWebViewScreen(onBack: () -> Unit) {
 
             WebView(
                 state = webViewState,
+                client = webViewClient,
                 onCreated = {
                     it.settings.javaScriptEnabled = true
                     it.setBackgroundColor(Color.TRANSPARENT)
@@ -57,5 +63,15 @@ fun EditProfileWebViewScreen(onBack: () -> Unit) {
                 Progress(modifier = Modifier.size(40.dp))
             }
         }
+    }
+}
+
+private class RestrictedWebViewClient : AccompanistWebViewClient() {
+    override fun shouldOverrideUrlLoading(
+        view: WebView?,
+        request: WebResourceRequest?
+    ): Boolean {
+        val clickedUrl = request?.url?.toString() ?: return false
+        return !clickedUrl.startsWith(EDIT_PROFILE_URL)
     }
 }
