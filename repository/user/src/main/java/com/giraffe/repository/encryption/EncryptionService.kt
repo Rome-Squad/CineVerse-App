82e4c4eeda8aa105ef5abe1ab9d79c01fe70c0f4
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-08-15 17:40:38 +0300

    catch exception in encryption service

--- repository/user/src/main/java/com/giraffe/repository/encryption/EncryptionService.kt
+++ repository/user/src/main/java/com/giraffe/repository/encryption/EncryptionService.kt
@@ -1,6 +1,7 @@
 package com.giraffe.repository.encryption
 
 
+import com.giraffe.repository.utils.mapToDomainException
 import com.giraffe.user.encryption.IEncryptionService
 import com.giraffe.user.encryption.IKeyStoreService
 import com.giraffe.user.encryption.ISecretKeyAliasEnum
@@ -21,14 +22,18 @@ class EncryptionService(
     }
 
     override fun decrypt(keyAlias: ISecretKeyAliasEnum, data: ByteArray): ByteArray {
-        val iv = data.copyOfRange(0, cipher.blockSize)
-        val encryptedData = data.copyOfRange(cipher.blockSize, data.size)
-        cipher.init(
-            Cipher.DECRYPT_MODE,
-            keyStoreService.getSecretKey(keyAlias.keyAlias),
-            IvParameterSpec(iv)
-        )
-        return cipher.doFinal(encryptedData)
+        return try {
+            val iv = data.copyOfRange(0, cipher.blockSize)
+            val encryptedData = data.copyOfRange(cipher.blockSize, data.size)
+            cipher.init(
+                Cipher.DECRYPT_MODE,
+                keyStoreService.getSecretKey(keyAlias.keyAlias),
+                IvParameterSpec(iv)
+            )
+            cipher.doFinal(encryptedData)
+        } catch (e: Exception) {
+            throw mapToDomainException(e)
+        }
     }
 
     override fun isSecretKeyExist(keyAlias: ISecretKeyAliasEnum): Boolean {
