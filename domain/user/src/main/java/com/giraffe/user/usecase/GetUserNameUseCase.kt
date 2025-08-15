1698edfe07ee383e1af8422b250ebfd959cd2ea9
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-08-15 23:15:02 +0300

    update to return flow

--- domain/user/src/main/java/com/giraffe/user/usecase/GetUserNameUseCase.kt
+++ domain/user/src/main/java/com/giraffe/user/usecase/GetUserNameUseCase.kt
@@ -1,17 +1,25 @@
 package com.giraffe.user.usecase
 
+import com.giraffe.user.repository.UserRepository
 import jakarta.inject.Inject
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class GetUserNameUseCase @Inject constructor(
-    private val getUserUseCase: GetUserUseCase
+    private val getUserUseCase: GetUserUseCase,
+    private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(): String {
-        val user = getUserUseCase.invoke()
-        val name = if (user.displayName.isEmpty()) {
-            user.username
-        } else {
-            user.displayName
+    operator fun invoke(): Flow<String> {
+        return getUserUseCase().map { user ->
+            if (user == null) {
+                userRepository.refreshUser().username
+            } else {
+                if (user.displayName.isEmpty()) {
+                    user.username
+                } else {
+                    user.displayName
+                }
+            }
         }
-        return name
     }
 }
