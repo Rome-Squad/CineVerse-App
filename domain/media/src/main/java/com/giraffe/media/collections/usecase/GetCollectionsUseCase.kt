6d3abbc8eb1505c0000fc398f2b78432c6572690
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-08-15 23:15:35 +0300

    update use cases that depend on get user use case

--- domain/media/src/main/java/com/giraffe/media/collections/usecase/GetCollectionsUseCase.kt
+++ domain/media/src/main/java/com/giraffe/media/collections/usecase/GetCollectionsUseCase.kt
@@ -1,15 +1,18 @@
 package com.giraffe.media.collections.usecase
 
-import com.giraffe.media.collections.repository.CollectionsRepository
-import com.giraffe.user.usecase.GetUserUseCase
-import javax.inject.Inject
 import com.giraffe.media.collections.entity.Collection
+import com.giraffe.media.collections.repository.CollectionsRepository
+import com.giraffe.user.exception.AccessDeniedException
+import com.giraffe.user.usecase.GetUserUseCase
+import kotlinx.coroutines.flow.first
+import javax.inject.Inject
+
 class GetCollectionsUseCase @Inject constructor(
     private val collectionsRepository: CollectionsRepository,
     private val getUserUseCase: GetUserUseCase
 ) {
     suspend operator fun invoke(): List<Collection> {
-        val accountId = getUserUseCase().id
+        val accountId = getUserUseCase().first()?.id ?: throw AccessDeniedException()
         return collectionsRepository.getCollections(accountId)
     }
 }
