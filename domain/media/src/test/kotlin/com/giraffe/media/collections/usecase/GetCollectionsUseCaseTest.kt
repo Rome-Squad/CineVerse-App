fcc378871239bcfe48bd05188a56a93905cd6c1e
AhmedMohamed056 <hamadarayyan056@gmail.com>
2025-08-16 05:35:23 +0300

    update get collections use case test

--- domain/media/src/test/kotlin/com/giraffe/media/collections/usecase/GetCollectionsUseCaseTest.kt
+++ domain/media/src/test/kotlin/com/giraffe/media/collections/usecase/GetCollectionsUseCaseTest.kt
@@ -2,9 +2,9 @@ package com.giraffe.media.collections.usecase
 
 import com.giraffe.media.collections.repository.CollectionsRepository
 import com.giraffe.media.collections.util.createFakeCollection
+import com.giraffe.user.entity.User
 import com.giraffe.user.usecase.GetUserUseCase
 import com.google.common.truth.Truth.assertThat
-import io.mockk.coEvery
 import io.mockk.every
 import io.mockk.mockk
 import kotlinx.coroutines.flow.first
@@ -20,19 +20,20 @@ class GetCollectionsUseCaseTest {
 
     @Test
     fun `should return list of collections when GetCollectionsUseCase invoked`() = runTest {
-        // Given
+
+        val fakeUser = User(id = 111, displayName = "test", username = "test", avatarUrl = null)
         val expectedCollections = listOf(
             createFakeCollection(id = 1, name = "Favorites"),
             createFakeCollection(id = 2, name = "Watch Later")
         )
 
-        coEvery { getUserUseCase().id } returns 111
-        every { collectionsRepository.getCollections(111) } returns flowOf(expectedCollections)
+        every { getUserUseCase() } returns flowOf(fakeUser)
+        every { collectionsRepository.getCollections(fakeUser.id) } returns flowOf(
+            expectedCollections
+        )
 
-        // When
         val result = getCollectionsUseCase().first()
 
-        // Then
         assertThat(result).isEqualTo(expectedCollections)
     }
 }
