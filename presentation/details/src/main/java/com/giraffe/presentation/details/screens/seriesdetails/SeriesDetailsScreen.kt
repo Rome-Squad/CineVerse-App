70666178848070337011f58ed2598b7a5b7f8abb
mohamedebrahem13 <mohamedebrahem1447@gmail.com>
2025-08-17 12:06:53 +0300

    use the model poster the series details

--- presentation/details/src/main/java/com/giraffe/presentation/details/screens/seriesdetails/SeriesDetailsScreen.kt
+++ presentation/details/src/main/java/com/giraffe/presentation/details/screens/seriesdetails/SeriesDetailsScreen.kt
@@ -28,7 +28,6 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.giraffe.designsystem.composable.InfoSection
-import com.giraffe.designsystem.composable.PosterListSection
 import com.giraffe.designsystem.composable.SectionTitle
 import com.giraffe.designsystem.theme.Theme
 import com.giraffe.presentation.details.R
@@ -36,6 +35,7 @@ import com.giraffe.presentation.details.base.BaseScreen
 import com.giraffe.presentation.details.components.GiveStarsBottomSheet
 import com.giraffe.presentation.details.components.LoginBottomSheet
 import com.giraffe.presentation.details.components.MainMovieOrSeriesDetails
+import com.giraffe.presentation.details.components.PosterListSection
 import com.giraffe.presentation.details.components.RatingSection
 import com.giraffe.presentation.details.components.ReviewCard
 import com.giraffe.presentation.details.components.SeasonCard
