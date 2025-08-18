c8d0e47014cbb9c9cf80d4acc9efe84607cedd90
Hend Sayed <hendsayed853@gmail.com>
2025-08-18 14:47:16 +0300

    Add guest user functionality to local data source

--- datasource/local/user/src/main/java/com/giraffe/user/AuthenticationLocalDataSourceImpl.kt
+++ datasource/local/user/src/main/java/com/giraffe/user/AuthenticationLocalDataSourceImpl.kt
@@ -6,7 +6,7 @@ import com.giraffe.user.util.safeCall
 import javax.inject.Inject
 
 class AuthenticationLocalDataSourceImpl @Inject constructor(
-    private val authenticationDatastore: AuthenticationDatastore
+    private val authenticationDatastore: AuthenticationDatastore,
 ) : AuthenticationLocalDataSource {
     override suspend fun saveSessionId(sessionId: String) =
         safeCall { authenticationDatastore.saveSessionId(sessionId) }
@@ -14,10 +14,16 @@ class AuthenticationLocalDataSourceImpl @Inject constructor(
     override suspend fun getSessionId(): String? =
         safeCall { authenticationDatastore.getSessionId() }
 
+    override suspend fun isLoggedInByAccount(): Boolean =
+        safeCall { authenticationDatastore.getSessionId() != null && !authenticationDatastore.isUserGuest() }
+
     override suspend fun isLoggedIn(): Boolean =
-        safeCall { authenticationDatastore.getSessionId() != null }
+        safeCall { authenticationDatastore.getSessionId() != null || authenticationDatastore.isUserGuest() }
 
     override suspend fun clearSessionId() =
         safeCall { authenticationDatastore.clearSessionId() }
+
+    override suspend fun setTheUserAsGuest() =
+        safeCall { authenticationDatastore.setTheUserAsGuest() }
 
 }
