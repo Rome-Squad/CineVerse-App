186a4d8542598e07e60f599a2c9c742595db0365
Hend Sayed <hendsayed853@gmail.com>
2025-08-18 14:47:32 +0300

    Refactor: Extract URL constants in UserApiService

--- datasource/remote/user/src/main/java/com/giraffe/user/retrofit/UserApiService.kt
+++ datasource/remote/user/src/main/java/com/giraffe/user/retrofit/UserApiService.kt
@@ -15,22 +15,29 @@ import retrofit2.http.Query
 
 interface UserApiService {
 
-    @GET("$AUTHENTICATION/token/new")
+    @GET(CREATE_REQUEST_TOKEN)
     suspend fun createRequestToken(): Response<RequestTokenResponse>
 
-    @POST("$AUTHENTICATION/token/validate_with_login")
+    @POST(VALIDATE_TOKEN_WITH_LOGIN_URL)
     suspend fun validateTokenWithLogin(@Body requestBody: TokenValidationBody): Response<RequestTokenResponse>
 
-    @POST("$AUTHENTICATION/session/new")
+    @POST(CREATE_SESSION_URL)
     suspend fun createSession(@Body requestBody: SessionRequestBody): Response<SessionResponse>
 
-    @GET("account")
-    suspend fun getUser(@Query("session_id") sessionId: String): Response<UserDto>
+    @GET(ACCOUNT)
+    suspend fun getUser(@Query(SESSION_ID) sessionId: String): Response<UserDto>
 
-    @HTTP(method = "DELETE", path = "authentication/session", hasBody = true)
+    @HTTP(method = DELETE, path = DELETE_SESSION_URL, hasBody = true)
     suspend fun deleteSession(@Body requestBody: DeleteSessionRequest): Response<Unit>
 
+
     companion object {
-        private const val AUTHENTICATION = "authentication"
+        private const val CREATE_REQUEST_TOKEN = "authentication/token/new"
+        private const val VALIDATE_TOKEN_WITH_LOGIN_URL = "authentication/token/validate_with_login"
+        private const val CREATE_SESSION_URL = "authentication/session/new"
+        private const val DELETE_SESSION_URL = "authentication/session"
+        private const val ACCOUNT = "account"
+        private const val SESSION_ID = "session_id"
+        private const val DELETE = "DELETE"
     }
 }
