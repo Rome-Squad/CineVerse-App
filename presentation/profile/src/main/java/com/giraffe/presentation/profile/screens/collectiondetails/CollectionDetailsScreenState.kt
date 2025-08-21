8023cc52e5a52a47fc64318eab4c03193ff08677
mostafa khaled <mostafakhaled2172003@gmail.com>
2025-08-21 06:33:47 +0300

    Integrate content preference in CollectionDetailsScreen
    This commit introduces content preference functionality to the CollectionDetailsScreen.
    
    Key changes:
    - Added `contentPreference` to `CollectionDetailsScreenState`.
    - `CollectionDetailsViewModel` now observes and updates `contentPreference` using `GetContentPreferenceUseCase`.
    - `CollectionDetailsScreen` passes `contentPreference` to the `SwipeableMoviePosterCard`.
    - Added a utility function `toStrengthLevel` to map `ContentPreference` to `StrengthLevel`.

--- presentation/profile/src/main/java/com/giraffe/presentation/profile/screens/collectiondetails/CollectionDetailsScreenState.kt
+++ presentation/profile/src/main/java/com/giraffe/presentation/profile/screens/collectiondetails/CollectionDetailsScreenState.kt
@@ -2,6 +2,7 @@ package com.giraffe.presentation.profile.screens.collectiondetails
 
 import com.giraffe.media.entity.Genre
 import com.giraffe.presentation.profile.model.SwipeablePoster
+import com.giraffe.user.entity.ContentPreference
 
 data class CollectionDetailsScreenState(
     val isLoading: Boolean = true,
@@ -11,4 +12,6 @@ data class CollectionDetailsScreenState(
     val collectionMovies: List<SwipeablePoster> = emptyList(),
     val movieGenres: List<Genre> = emptyList(),
     val isDeleteTipVisible: Boolean = true,
-)
+    val contentPreference: ContentPreference = ContentPreference.HIDE_EXPLICIT,
+
+    )
